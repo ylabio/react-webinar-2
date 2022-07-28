ef300e9819bfd4064775084d08f70469a1116ee8
Viktoriya Marysheva <programvika@gmail.com>
2022-07-28 22:20:02 +0300

    Создала счетчик и изменила логику selectItem

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {counter} from './utils.js';
 import './style.css';
 
@@ -11,32 +11,41 @@ function App({store}) {
   // Выбор состояния из store
   const {items} = store.getState();
 
+  const checkSelect = (code) => {
+    store.selectItem(code);
+  }
+
   return (
-    <div className='App'>
-      <div className='App__head'>
+    <div className="App">
+      <div className="App__head">
         <h1>Приложение на чистом JS</h1>
       </div>
-      <div className='Controls'>
-        <button onClick={() => {
-          const code = counter();
-          store.createItem({code, title: `Новая запись ${code}`})
-        }}> Добавить </button>
+      <div className="Controls">
+        <button
+          onClick={() => {
+            const code = counter();
+            store.createItem({ code, title: `Новая запись` });
+          }}>
+          {' '}
+          Добавить
+        </button>
       </div>
-      <div className='App__center'>
-        <div className='List'>{items.map(item =>
-          <div key={item.code} className='List__item'>
-            <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-                 onClick={() => store.selectItem(item.code)}>
-              <div className='Item__number'>{item.code}</div>
-              <div className='Item__title'>{item.title}</div>
-              <div className='Item__actions'>
-                <button onClick={() => store.deleteItem(item.code)}>
-                  Удалить
-                </button>
+      <div className="App__center">
+        <div className="List">
+          {items.map((item) => (
+            <div key={item.code} className="List__item">
+              <div
+                className={'Item' + (item.selected ? ' Item_selected' : '')}
+                onClick={() => checkSelect(item.code)}>
+                <div className="Item__number">{item.code}</div>
+                <div className="Item__title">{item.title}</div>
+                <div>{item.count > 0 && <div>Выделялось {item.count} раз</div>} </div>
+                <div className="Item__actions">
+                  <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
+                </div>
               </div>
             </div>
-          </div>
-        )}
+          ))}
         </div>
       </div>
     </div>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,4 @@
 class Store {
-
   constructor(initState) {
     // Состояние приложения (данные)
     this.state = initState;
@@ -36,17 +35,17 @@ class Store {
     this.listners.push(callback);
     // Возвращаем функцию для удаления слушателя
     return () => {
-      this.listners = this.listners.filter(item => item !== callback);
-    }
+      this.listners = this.listners.filter((item) => item !== callback);
+    };
   }
 
   /**
    * Создание записи
    */
-  createItem({code, title = 'Новая запись', selected = false}) {
+  createItem({ code, title = 'Новая запись', selected = false, count = 0 }) {
     this.setState({
       ...this.state,
-      items: this.state.items.concat({code, title, selected})
+      items: this.state.items.concat({ code, title, selected, count }),
     });
   }
 
@@ -57,7 +56,7 @@ class Store {
   deleteItem(code) {
     this.setState({
       ...this.state,
-      items: this.state.items.filter(item => item.code !== code)
+      items: this.state.items.filter((item) => item.code !== code),
     });
   }
 
@@ -65,17 +64,21 @@ class Store {
    * Выделение записи по её коду
    * @param code
    */
-  selectItem(code) {
+  selectItem(id) {
     this.setState({
       ...this.state,
-      items: this.state.items.map(item => {
-        if (item.code === code){
-          item.selected = !item.selected;
+      items: this.state.items.map((item) => {
+        if (item.code === id) {
+          item.count++;
+          item.selected = true;
+        } else {
+          item.selected = false;
         }
         return item;
-      })
+      }),
     });
   }
+
 }
 
 export default Store;

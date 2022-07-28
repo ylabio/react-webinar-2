5c8324caf4e640bcbae6a1dd9e59bf1fbcd56195
fogein <77330295+fogein@users.noreply.github.com>
2022-07-28 23:13:24 +0300

    finish

---
+++ src/Comp/item.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../style.css';
+
+export const Item = (props) => {
+  return (
+    <div key={props.code} className='List__item'>
+    <div type={'text'} className={'Item' + (props.selected ? ' Item_selected' : '' )}
+         onClick={() => props.store.selectItem(props.code)}>
+      <div className='Item__number'>{props.code}</div>
+      <div className='Item__title'>{props.title} &nbsp;
+        <span>{props.counter > 0 ? <span>Выделялось {props.counter} раз</span>: <span></span>}</span>
+      </div>
+      <div className='Item__actions'>
+        <button onClick={() => props.store.deleteItem(props.code)}>
+          Удалить
+        </button>
+      </div>
+    </div>
+  </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -2,24 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.js';
 import Store from "./store.js";
-import {counter} from './utils.js';
+import { counter } from './utils.js';
 
 const store = new Store({
   items: [
-    {code: counter(), title: 'Название элемента'},
-    {code: counter(), title: 'Некий объект'},
-    {code: counter(), title: 'Заголовок'},
-    {code: counter(), title: 'Короткое название'},
-    {code: counter(), title: 'Запись'},
-    {code: counter(), title: 'Пример выделенной записи', selected: true},
-    {code: counter(), title: 'Седьмой'},
+    { code: counter(), title: 'Название элемента', selected: false, counter: 0  },
+    { code: counter(), title: 'Некий объект', selected: false, counter: 0 },
+    { code: counter(), title: 'Заголовок', selected: false, counter: 0 },
+    { code: counter(), title: 'Короткое название', selected: false, counter: 0 },
+    { code: counter(), title: 'Запись', selected: false, counter: 0 },
+    { code: counter(), title: 'Пример выделенной записи', selected: false, counter: 0 },
+    { code: counter(), title: 'Седьмой', selected: false, counter: 0 },
   ]
 });
 
 // Реакция на изменение store - повторный рендер приложения
 store.subscribe(() => {
-  ReactDOM.render(<App store={store}/>, document.body);
+  ReactDOM.render(<App store={store} />, document.body);
 });
 
 // Первый рендер (один раз)
-ReactDOM.render(<App store={store}/>, document.body);
+ReactDOM.render(<App store={store} />, document.body);

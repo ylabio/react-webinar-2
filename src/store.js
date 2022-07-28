f8fe6107ae893fc4eb32eee7e5cdeab3206293c4
OBOLLL20333 <97986329+OBOLLL20333@users.noreply.github.com>
2022-07-29 01:08:08 +0300

    Add files via upload

--- src/store.js
+++ src/store.js
@@ -1,81 +1,84 @@
 class Store {
 
-  constructor(initState) {
-    // Состояние приложения (данные)
-    this.state = initState;
-    // Слушатели изменений state
-    this.listners = [];
-  }
+	constructor(initState) {
+		// Состояние приложения (данные)
+		this.state = initState;
+		// Слушатели изменений state
+		this.listners = [];
+	}
 
-  /**
-   * Выбор state
-   * @return {Object}
-   */
-  getState() {
-    return this.state;
-  }
+	/**
+	 * Выбор state
+	 * @return {Object}
+	 */
+	getState() {
+		return this.state;
+	}
 
-  /**
-   * Установка state
-   * @param newState {Object}
-   */
-  setState(newState) {
-    this.state = newState;
-    // Оповещаем всех подписчиков об изменении стейта
-    for (const lister of this.listners) {
-      lister();
-    }
-  }
+	/**
+	 * Установка state
+	 * @param newState {Object}
+	 */
+	setState(newState) {
+		this.state = newState;
+		// Оповещаем всех подписчиков об изменении стейта
+		for (const lister of this.listners) {
+			lister();
+		}
+	}
 
-  /**
-   * Подписка на изменение state
-   * @param callback {Function}
-   * @return {Function} Функция для отписки
-   */
-  subscribe(callback) {
-    this.listners.push(callback);
-    // Возвращаем функцию для удаления слушателя
-    return () => {
-      this.listners = this.listners.filter(item => item !== callback);
-    }
-  }
+	/**
+	 * Подписка на изменение state
+	 * @param callback {Function}
+	 * @return {Function} Функция для отписки
+	 */
+	subscribe(callback) {
+		this.listners.push(callback);
+		// Возвращаем функцию для удаления слушателя
+		return () => {
+			this.listners = this.listners.filter(item => item !== callback);
+		}
+	}
 
-  /**
-   * Создание записи
-   */
-  createItem({code, title = 'Новая запись', selected = false}) {
-    this.setState({
-      ...this.state,
-      items: this.state.items.concat({code, title, selected})
-    });
-  }
+	/**
+	 * Создание записи
+	 */
+	createItem({ code, title = 'Новая запись', selected = false, count = 0 }) {
+		this.setState({
+			...this.state,
+			items: this.state.items.concat({ code, title, selected, count })
+		});
+	}
 
-  /**
-   * Удаление записи по её коду
-   * @param code
-   */
-  deleteItem(code) {
-    this.setState({
-      ...this.state,
-      items: this.state.items.filter(item => item.code !== code)
-    });
-  }
+	/**
+	 * Удаление записи по её коду
+	 * @param code
+	 */
+	deleteItem(code) {
+		this.setState({
+			...this.state,
+			items: this.state.items.filter(item => item.code !== code)
+		});
+	}
 
-  /**
-   * Выделение записи по её коду
-   * @param code
-   */
-  selectItem(code) {
-    this.setState({
-      ...this.state,
-      items: this.state.items.map(item => {
-        if (item.code === code){
-          item.selected = !item.selected;
-        }
-        return item;
-      })
-    });
-  }
+	/**
+	 * Выделение записи по её коду
+	 * @param code
+	 */
+	selectItem(code) {
+		this.setState({
+			...this.state,
+			items: this.state.items.map(item => {
+				if (item.code === code) {
+					item.selected = !item.selected;
+					item.selected ? item.count++ : item.count;
+				} else {
+					item.selected = false;
+				}
+				return item;
+			})
+		});
+	}
 }
 
 export default Store;

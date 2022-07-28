65f45180e5d7e0c6e9ce2fbfb0d38f99385adce3
Borodin Alexandr <bvachtang@gmail.com>
2022-07-28 23:23:44 +0300

    попытка номер раз

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {counter} from './utils.js';
+import { counter } from './utils.js';
 import './style.css';
 
 /**
@@ -7,36 +7,66 @@ import './style.css';
  * @param store {Store} Состояние приложения
  * @return {React.ReactElement} Виртуальные элементы React
  */
-function App({store}) {
+function App({ store }) {
   // Выбор состояния из store
-  const {items} = store.getState();
+  const { items } = store.getState();
+
+  const activeListClass = 'Item_selected';
+
+  const disabler = (e, code) => {
+    items.forEach((item) => {
+      if (item.code !== code) {
+        item.selected = false;
+      }
+    });
+  };
 
   return (
-    <div className='App'>
-      <div className='App__head'>
+    <div className="App">
+      <div className="App__head">
         <h1>Приложение на чистом JS</h1>
       </div>
-      <div className='Controls'>
-        <button onClick={() => {
-          const code = counter();
-          store.createItem({code, title: `Новая запись ${code}`})
-        }}> Добавить </button>
+      <div className="Controls">
+        <button
+          onClick={() => {
+            const code = counter();
+            store.createItem({ code, title: `Новая запись ${code}` });
+          }}
+        >
+          {' '}
+          Добавить{' '}
+        </button>
       </div>
-      <div className='App__center'>
-        <div className='List'>{items.map(item =>
-          <div key={item.code} className='List__item'>
-            <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-                 onClick={() => store.selectItem(item.code)}>
-              <div className='Item__number'>{item.code}</div>
-              <div className='Item__title'>{item.title}</div>
-              <div className='Item__actions'>
-                <button onClick={() => store.deleteItem(item.code)}>
-                  Удалить
-                </button>
+      <div className="App__center">
+        <div className="List">
+          {items.map((item) => {
+            const [listCounter, setListCounter] = React.useState(0);
+            return (
+              <div key={item.code} className="List__item">
+                <div
+                  className={item.selected ? `Item ${activeListClass}` : 'Item'}
+                  onClick={(e) => {
+                    disabler(e, item.code);
+                    store.selectItem(item.code);
+                    item.selected
+                      ? setListCounter(listCounter + 1)
+                      : setListCounter(listCounter);
+                  }}
+                >
+                  <div className="Item__number">{item.code}</div>
+                  <div className="Item__title">{item.title}</div>
+                  <div className="Item__counter">
+                    {listCounter === 0 ? '' : `Выделялось ${listCounter} раз`}
+                  </div>
+                  <div className="Item__actions">
+                    <button onClick={() => store.deleteItem(item.code)}>
+                      Удалить
+                    </button>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        )}
+            );
+          })}
         </div>
       </div>
     </div>

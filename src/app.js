87bbc6167306f7f9ef5761f2bd5b117f8dfbc0fc
vladimirluk88 <vlukinskii82@gmail.com>
2022-07-28 21:11:56 +0300

    added select counter and defocus

--- src/app.js
+++ src/app.js
@@ -28,7 +28,9 @@ function App({store}) {
             <div className={'Item' + (item.selected ? ' Item_selected' : '')}
                  onClick={() => store.selectItem(item.code)}>
               <div className='Item__number'>{item.code}</div>
-              <div className='Item__title'>{item.title}</div>
+              <div className='Item__title'>{item.title}
+              {item.selectCounter ?
+              <div className="Item__select-counter">Выделяется {item.selectCounter} раз</div> : <></>}</div>
               <div className='Item__actions'>
                 <button onClick={() => store.deleteItem(item.code)}>
                   Удалить

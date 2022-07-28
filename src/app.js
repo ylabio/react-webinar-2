16adf4e45c265cb1e48ed07c5550f3c6de0a18c6
Anton <antonshvetswork@yandex.ru>
2022-07-28 20:59:07 +0300

    Выполнено задание lecture1

--- src/app.js
+++ src/app.js
@@ -10,33 +10,47 @@ import './style.css';
 function App({store}) {
   // Выбор состояния из store
   const {items} = store.getState();
-
   return (
-    <div className='App'>
-      <div className='App__head'>
+    <div className="App">
+      <div className="App__head">
         <h1>Приложение на чистом JS</h1>
       </div>
-      <div className='Controls'>
-        <button onClick={() => {
-          const code = counter();
-          store.createItem({code, title: `Новая запись ${code}`})
-        }}> Добавить </button>
+      <div className="Controls">
+        <button
+          onClick={() => {
+            const code = counter();
+            store.createItem({ code, title: `Новая запись ${code}` });
+          }}
+        >
+          {" "}
+          Добавить{" "}
+        </button>
       </div>
-      <div className='App__center'>
-        <div className='List'>{items.map(item =>
-          <div key={item.code} className='List__item'>
-            <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-                 onClick={() => store.selectItem(item.code)}>
-              <div className='Item__number'>{item.code}</div>
-              <div className='Item__title'>{item.title}</div>
-              <div className='Item__actions'>
-                <button onClick={() => store.deleteItem(item.code)}>
-                  Удалить
-                </button>
+      <div className="App__center">
+        <div className="List">
+          {items.map((item) => (
+            <div key={item.code} className="List__item">
+              <div
+                className={"Item" + (item.selected ? " Item_selected" : "")}
+                onClick={() => {
+                  store.selectItem(item.code);
+                  store.countItem(item.code);
+                }}
+              >
+                <div className="Item__number">{item.code}</div>
+                <div className="Item__title">
+                  {item.title} 
+                  {item.count ? ` | Выделялся ${item.count} раз` : ""}
+                </div>
+                <div className="Item__count"></div>
+                <div className="Item__actions">
+                  <button onClick={() => store.deleteItem(item.code)}>
+                    Удалить
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        )}
+          ))}
         </div>
       </div>
     </div>

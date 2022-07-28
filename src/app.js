deb80b947aad381f9cb2dc9fa78f921699f58a3d
Anderboi <upolovnew@gmail.com>
2022-07-28 22:11:21 +0300

    Select Counter added. Item selection fixed

--- src/app.js
+++ src/app.js
@@ -1,46 +1,69 @@
-import React from 'react';
-import {counter} from './utils.js';
-import './style.css';
+import React from "react";
+import { counter } from "./utils.js";
+import "./style.css";
 
 /**
  * Приложение
  * @param store {Store} Состояние приложения
  * @return {React.ReactElement} Виртуальные элементы React
  */
-function App({store}) {
+const App = ({ store }) => {
   // Выбор состояния из store
-  const {items} = store.getState();
+  const { items } = store.getState();
 
   return (
-    <div className='App'>
-      <div className='App__head'>
+    <div className="App">
+      <div className="App__head">
         <h1>Приложение на чистом JS</h1>
       </div>
-      <div className='Controls'>
-        <button onClick={() => {
-          const code = counter();
-          store.createItem({code, title: `Новая запись ${code}`})
-        }}> Добавить </button>
+      <div className="Controls">
+        <button
+          onClick={() => {
+            const code = counter();
+            store.createItem({ code, title: `Новая запись ${code}` });
+          }}
+        >
+          {" "}
+          Добавить{" "}
+        </button>
       </div>
-      <div className='App__center'>
-        <div className='List'>{items.map(item =>
-          <div key={item.code} className='List__item'>
-            <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-                 onClick={() => store.selectItem(item.code)}>
-              <div className='Item__number'>{item.code}</div>
-              <div className='Item__title'>{item.title}</div>
-              <div className='Item__actions'>
-                <button onClick={() => store.deleteItem(item.code)}>
-                  Удалить
-                </button>
+      <div className="App__center">
+        <div className="List">
+          {items.map((item) => (
+            <div key={item.code} className="List__item">
+              <div
+                className={"Item" + (item.selected ? " Item_selected" : "")}
+                onClick={() => store.selectItem(item.code)}
+              >
+                <div className="Item__number">{item.code}</div>
+                <div className="Item__title">
+                  {item.title}
+                  {item.selectCount !== 0 &&
+                    ` | Выделялся ${item.selectCount} 
+                  ${
+                    (item.selectCount % 10 === 2 ||
+                      item.selectCount % 10 === 3 ||
+                      item.selectCount % 10 === 4) &&
+                    item.selectCount !== 12 &&
+                    item.selectCount !== 13 &&
+                    item.selectCount !== 14
+                      ? "раза"
+                      : "раз"
+                  }`}
+                </div>
+                <div className="Item__selectCounter"></div>
+                <div className="Item__actions">
+                  <button onClick={() => store.deleteItem(item.code)}>
+                    Удалить
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        )}
+          ))}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;

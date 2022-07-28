bd177bd346dd9fabbd7b168e43e9aed8f3495ce2
Anton Ryabtsev <anton_ryabcev@mail.ru>
2022-07-29 00:36:55 +0300

    modified the function selectItem in store

--- src/store.js
+++ src/store.js
@@ -7,7 +7,7 @@ class Store {
     this.listners = [];
   }
 
-  /**
+  /** 
    * Выбор state
    * @return {Object}
    */
@@ -69,8 +69,10 @@ class Store {
     this.setState({
       ...this.state,
       items: this.state.items.map(item => {
-        if (item.code === code){
+        if (item.code === code ){
           item.selected = !item.selected;
+        } else {
+          item.selected = false;
         }
         return item;
       })

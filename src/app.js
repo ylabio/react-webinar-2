b9be33b166721b1b4a4f13aaa6badd9196d9a18c
spnikit <spnikit@gmail.com>
2022-07-29 01:13:23 +0300

    extract ListItem to separate component

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { counter } from "./utils.js";
 import "./style.css";
+import ListItem from "./components/ListItem.js";
 
 /**
  * Приложение
@@ -30,29 +31,7 @@ function App({ store }) {
       <div className="App__center">
         <div className="List">
           {items.map((item) => (
-            <div key={item.code} className="List__item">
-              <div
-                className={"Item" + (item.selected ? " Item_selected" : "")}
-                onClick={() => store.selectItem(item.code)}
-              >
-                <div className="Item__number">{item.code}</div>
-                <div className="Item__title">
-                  {item.title}{" "}
-                  {item.numberOfSelections && (
-                    <span className="Item__number-of-selections">
-                      | Выделялся {item.numberOfSelections}
-                      {item.numberOfSelections > 1 && item.numberOfSelections < 5
-                        ? " раза"
-                        : " раз"}
-                    </span>
-                  )}
-                </div>
-
-                <div className="Item__actions">
-                  <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
-                </div>
-              </div>
-            </div>
+            <ListItem item={item} store={store} />
           ))}
         </div>
       </div>
---
+++ src/components/ListItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function ListItem({ item, store }) {
+  return (
+    <div key={item.code} className="List__item">
+      <div
+        className={"Item" + (item.selected ? " Item_selected" : "")}
+        onClick={() => store.selectItem(item.code)}
+      >
+        <div className="Item__number">{item.code}</div>
+        <div className="Item__title">
+          {item.title}{" "}
+          {item.numberOfSelections && (
+            <span className="Item__number-of-selections">
+              | Выделялся {item.numberOfSelections}
+              {item.numberOfSelections > 1 && item.numberOfSelections < 5 ? " раза" : " раз"}
+            </span>
+          )}
+        </div>
+
+        <div className="Item__actions">
+          <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ListItem;

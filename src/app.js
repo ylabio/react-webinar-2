3b14231f6eb604e590af3dd623e5a6247780ee65
Irina <myr-irina2@yandex.ru>
2022-07-29 18:36:07 +0300

    Добавила количество выделений пункта

--- src/app.js
+++ src/app.js
@@ -18,9 +18,7 @@ function App({store}) {
       </div>
       <div className='Controls'>
         <button onClick={() => {
-          const code = counter();
-          console.log(code)
-
+          const code = counter();       
           store.createItem({code, title: `Новая запись ${code}`})
         }}> Добавить </button>
       </div>
@@ -30,7 +28,7 @@ function App({store}) {
             <div className={'Item' + (item.selected ? ' Item_selected' : '')}
                  onClick={() => store.selectItem(item.code)}>
               <div className='Item__number'>{item.code}</div>
-              <div className='Item__title'>{item.title}</div>
+              <div className='Item__title'>{item.title} {`Выделялось ${item.clickCount} раз(-а)`}</div>
               <div className='Item__actions'>
                 <button onClick={() => store.deleteItem(item.code)}>
                   Удалить

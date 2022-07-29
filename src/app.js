539d7cee8e5b3a87bf5db8752515e829b2323546
Irina <myr-irina2@yandex.ru>
2022-07-29 18:43:57 +0300

    Убрала вывод 0.

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ function App({store}) {
             <div className={'Item' + (item.selected ? ' Item_selected' : '')}
                  onClick={() => store.selectItem(item.code)}>
               <div className='Item__number'>{item.code}</div>
-              <div className='Item__title'>{item.title} {`Выделялось ${item.clickCount} раз(-а)`}</div>
+              <div className='Item__title'>{item.title} {`Выделялось ${item.clickCount === 0 ? '' : item.clickCount} раз(-а)`}</div>
               <div className='Item__actions'>
                 <button onClick={() => store.deleteItem(item.code)}>
                   Удалить

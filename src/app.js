cb155d844f1707753be631eb281b68a0cf58ec3a
Alexandr <k.a.r.2002@mail.ru>
2022-07-29 22:18:56 +0300

    counter click

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {counter} from './utils.js';
+import { counter } from './utils.js';
 import './style.css';
 
 /**
@@ -7,9 +7,9 @@ import './style.css';
  * @param store {Store} Состояние приложения
  * @return {React.ReactElement} Виртуальные элементы React
  */
-function App({store}) {
+function App({ store }) {
   // Выбор состояния из store
-  const {items} = store.getState();
+  const { items } = store.getState();
 
   return (
     <div className='App'>
@@ -19,21 +19,23 @@ function App({store}) {
       <div className='Controls'>
         <button onClick={() => {
           const code = counter();
-          store.createItem({code, title: `Новая запись ${code}`})
+          store.createItem({ code, title: `Новая запись ${code}` })
         }}> Добавить </button>
       </div>
       <div className='App__center'>
         <div className='List'>{items.map(item =>
           <div key={item.code} className='List__item'>
             <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-                 onClick={() => store.selectItem(item.code)}>
+              onClick={() => store.selectItem(item.code)}>
               <div className='Item__number'>{item.code}</div>
-              <div className='Item__title'>{item.title}</div>
+              <div className='Item__title'>{item.title + (item.counterClick > 0 ? ` || Выделялось ${item.counterClick} раз` : '')}</div>
+
               <div className='Item__actions'>
                 <button onClick={() => store.deleteItem(item.code)}>
                   Удалить
                 </button>
               </div>
+
             </div>
           </div>
         )}
--- src/store.js
+++ src/store.js
@@ -43,10 +43,10 @@ class Store {
   /**
    * Создание записи
    */
-  createItem({code, title = 'Новая запись', selected = false}) {
+  createItem({ code, title = 'Новая запись', selected = false, counterClick = 0 }) {
     this.setState({
       ...this.state,
-      items: this.state.items.concat({code, title, selected})
+      items: this.state.items.concat({ code, title, selected, counterClick })
     });
   }
 
@@ -66,11 +66,17 @@ class Store {
    * @param code
    */
   selectItem(code) {
+
     this.setState({
       ...this.state,
       items: this.state.items.map(item => {
-        if (item.code === code){
+        if(item.code!==code){
+          item.selected = false;
+        }
+        if (item.code === code) {
           item.selected = !item.selected;
+          item.selected===true?item.counterClick++:null 
+          
         }
         return item;
       })

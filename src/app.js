a7667680597aa34e43faa8ce8041f8b72bab33c2
tsy <tsy@mail.com>
2022-07-29 12:29:55 +0300

    add: home work Lecture1

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import {counter} from './utils.js';
+import React, { useState } from 'react';
+import { counter } from './utils.js';
 import './style.css';
+import { render } from 'react-dom';
 
 /**
  * Приложение
  * @param store {Store} Состояние приложения
  * @return {React.ReactElement} Виртуальные элементы React
  */
-function App({store}) {
+function App({ store }) {
   // Выбор состояния из store
-  const {items} = store.getState();
+  const { items } = store.getState();
+  const [count, setCount] = useState({});
+
+  function handleClick(id, selected) {
+    if (count[id] && selected) {
+      setCount(prev => ({ ...prev, [id]: count[id] + 1 }))
+    } else {
+      setCount(prev => ({ ...prev, [id]: 1 }))
+    }
+  }
 
   return (
     <div className='App'>
@@ -19,16 +29,20 @@ function App({store}) {
       <div className='Controls'>
         <button onClick={() => {
           const code = counter();
-          store.createItem({code, title: `Новая запись ${code}`})
+          store.createItem({ code, title: `Новая запись ${code}` })
         }}> Добавить </button>
       </div>
       <div className='App__center'>
         <div className='List'>{items.map(item =>
-          <div key={item.code} className='List__item'>
+          <div key={item.code} onClick={() => handleClick(item.code, item.selected)} className='List__item'>
             <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-                 onClick={() => store.selectItem(item.code)}>
+              onClick={() => {
+                store.selectItem(item.code)
+
+              }}>
+
               <div className='Item__number'>{item.code}</div>
-              <div className='Item__title'>{item.title}</div>
+              <div className='Item__title'>{item.title}  {count[item.code] ? ' ! Выделялся ' + count[item.code] + ' раз' : ''}</div>
               <div className='Item__actions'>
                 <button onClick={() => store.deleteItem(item.code)}>
                   Удалить

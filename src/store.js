375936a76c418b4f7ad7366dde5e299d39037516
Alexander Moryan <a.dencyh@gmail.com>
2022-07-29 11:23:26 +0300

    task highlight only one and count info

--- src/store.js
+++ src/store.js
@@ -43,10 +43,10 @@ class Store {
   /**
    * Создание записи
    */
-  createItem({code, title = 'Новая запись', selected = false}) {
+  createItem({code, title = 'Новая запись', selected = false, highlighted}) {
     this.setState({
       ...this.state,
-      items: this.state.items.concat({code, title, selected})
+      items: this.state.items.concat({code, title, selected, highlighted})
     });
   }
 
@@ -69,8 +69,17 @@ class Store {
     this.setState({
       ...this.state,
       items: this.state.items.map(item => {
-        if (item.code === code){
+        if (item.code === code && !item.selected){
           item.selected = !item.selected;
+
+          if (item.highlighted) 
+            item.highlighted++;
+          else 
+            item.highlighted = 1;
+
+        } else {
+          // Отмена выделения, если элемент не таргет клика
+          item.selected = false
         }
         return item;
       })

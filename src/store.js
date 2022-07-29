2cb5f55cc2abea1fc927e251babb9d9cf0cb88a2
evgeny3322 <retard_x_98@mail.ru>
2022-07-29 16:37:07 +0300

    HW1

--- src/store.js
+++ src/store.js
@@ -43,10 +43,10 @@ class Store {
   /**
    * Создание записи
    */
-  createItem({code, title = 'Новая запись', selected = false}) {
+  createItem({ code, title = 'Новая запись', counter = 0, selected = false }) {
     this.setState({
       ...this.state,
-      items: this.state.items.concat({code, title, selected})
+      items: this.state.items.concat({ code, title, counter, selected })
     });
   }
 
@@ -69,8 +69,10 @@ class Store {
     this.setState({
       ...this.state,
       items: this.state.items.map(item => {
-        if (item.code === code){
+        item.selected = '';
+        if (item.code === code) {
           item.selected = !item.selected;
+          item.counter++;
         }
         return item;
       })

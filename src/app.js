ff1edbac4788b41e33828f5c7bbe43f84370689e
NikitaSchreyder <nikitaschreyder@gmail.com>
2022-07-29 11:30:29 +0500

    Add select counter

--- src/app.js
+++ src/app.js
@@ -11,32 +11,46 @@ function App({store}) {
   // Выбор состояния из store
   const {items} = store.getState();
 
+  const addItem = () => {
+    const code = counter();
+    store.createItem({code, title: `Новая запись ${code}`})
+  }
+
+  // Оработка клика 
+  const selectItemHandler = (code) => {
+    store.selectItem(code);
+    store.incremetSelectCounter(code);
+  }
+
+  const itemsList = items.map(item => 
+    <div key={item.code} className='List__item'>
+      <div 
+        className={'Item' + (item.selected ? ' Item_selected' : '')}
+        onClick={() => selectItemHandler(item.code)}
+      >
+        <div className='Item__number'>{item.code}</div>
+        <div className='Item__title'>
+          {item.title}
+          {item.selectCount > 0 && ` | Выделялся ${item.selectCount} раз`}
+        </div>
+        <div className='Item__actions'>
+          <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
+        </div>
+      </div>
+    </div>
+  )
+
   return (
     <div className='App'>
       <div className='App__head'>
         <h1>Приложение на чистом JS</h1>
       </div>
       <div className='Controls'>
-        <button onClick={() => {
-          const code = counter();
-          store.createItem({code, title: `Новая запись ${code}`})
-        }}> Добавить </button>
+        <button onClick={addItem}>Добавить</button>
       </div>
       <div className='App__center'>
-        <div className='List'>{items.map(item =>
-          <div key={item.code} className='List__item'>
-            <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-                 onClick={() => store.selectItem(item.code)}>
-              <div className='Item__number'>{item.code}</div>
-              <div className='Item__title'>{item.title}</div>
-              <div className='Item__actions'>
-                <button onClick={() => store.deleteItem(item.code)}>
-                  Удалить
-                </button>
-              </div>
-            </div>
-          </div>
-        )}
+        <div className='List'>
+          {itemsList}
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import {counter} from './utils.js';
 
 const store = new Store({
   items: [
-    {code: counter(), title: 'Название элемента'},
-    {code: counter(), title: 'Некий объект'},
-    {code: counter(), title: 'Заголовок'},
-    {code: counter(), title: 'Короткое название'},
-    {code: counter(), title: 'Запись'},
-    {code: counter(), title: 'Пример выделенной записи', selected: true},
-    {code: counter(), title: 'Седьмой'},
+    {code: counter(), title: 'Название элемента', selectCount: 0},
+    {code: counter(), title: 'Некий объект', selectCount: 0},
+    {code: counter(), title: 'Заголовок', selectCount: 0},
+    {code: counter(), title: 'Короткое название', selectCount: 0},
+    {code: counter(), title: 'Запись', selectCount: 0},
+    {code: counter(), title: 'Пример выделенной записи', selectCount: 0},
+    {code: counter(), title: 'Седьмой', selectCount: 0},
   ]
 });
 

4c849f2c95e85ab3c8004e311f45ea4834004169
home <livanovandrej99@gmail.com>
2022-07-29 19:26:18 +0700

    hw_1

--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ import Store from "./store.js";
 import {counter} from './utils.js';
 
 const store = new Store({
-  items: [
-    {code: counter(), title: 'Название элемента'},
-    {code: counter(), title: 'Некий объект'},
-    {code: counter(), title: 'Заголовок'},
-    {code: counter(), title: 'Короткое название'},
-    {code: counter(), title: 'Запись'},
-    {code: counter(), title: 'Пример выделенной записи', selected: true},
-    {code: counter(), title: 'Седьмой'},
-  ]
+    items: [
+        {code: counter(), title: 'Название элемента', clickCount: 0},
+        {code: counter(), title: 'Некий объект', clickCount: 0},
+        {code: counter(), title: 'Заголовок', clickCount: 0},
+        {code: counter(), title: 'Короткое название', clickCount: 0},
+        {code: counter(), title: 'Запись', clickCount: 0},
+        {code: counter(), title: 'Пример выделенной записи', selected: true, clickCount: 0},
+        {code: counter(), title: 'Седьмой', clickCount: 0},
+    ]
 });
 
 // Реакция на изменение store - повторный рендер приложения
 store.subscribe(() => {
-  ReactDOM.render(<App store={store}/>, document.body);
+    ReactDOM.render(<App store={store}/>, document.body);
 });
 
 // Первый рендер (один раз)

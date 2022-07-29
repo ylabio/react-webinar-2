294d0efbbf0bcc8fd5cc89d1ab3061c12b9e3129
EugeneKlim <greedmaster7799@mail.ru>
2022-07-29 11:47:36 +0300

    lecture1

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import {counter} from './utils.js';
 
 const store = new Store({
   items: [
-    {code: counter(), title: 'Название элемента'},
-    {code: counter(), title: 'Некий объект'},
-    {code: counter(), title: 'Заголовок'},
-    {code: counter(), title: 'Короткое название'},
-    {code: counter(), title: 'Запись'},
-    {code: counter(), title: 'Пример выделенной записи', selected: true},
-    {code: counter(), title: 'Седьмой'},
+    {code: counter(), title: 'Название элемента', click: 0},
+    {code: counter(), title: 'Некий объект', click: 0},
+    {code: counter(), title: 'Заголовок', click: 0},
+    {code: counter(), title: 'Короткое название', click: 0},
+    {code: counter(), title: 'Запись', click: 0},
+    {code: counter(), title: 'Пример выделенной записи', click: 0, selected: true},
+    {code: counter(), title: 'Седьмой', click: 0},
   ]
 });
 

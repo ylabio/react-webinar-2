7b34e4636f65d116e9978b744f16926610311c40
robert <78978033+robertGGA@users.noreply.github.com>
2022-07-29 20:27:19 +0500

    added counter and removed all selected items

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ function App({store}) {
             <div className={'Item' + (item.selected ? ' Item_selected' : '')}
                  onClick={() => store.selectItem(item.code)}>
               <div className='Item__number'>{item.code}</div>
-              <div className='Item__title'>{item.title}</div>
+              <div className='Item__title'>{item.title} {item.counter !== 0 ? ` | Выделялось ${item.counter} раз` : ''}</div>
               <div className='Item__actions'>
                 <button onClick={() => store.deleteItem(item.code)}>
                   Удалить

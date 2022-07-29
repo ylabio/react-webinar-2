05c12b003af87b8e98f453a41360e4aadf119e70
AlexandraAlekseyuk <alex.alekseyuk@gmail.com>
2022-07-29 15:43:08 +0300

    Lecture1-HomeWork

--- src/store.js
+++ src/store.js
@@ -43,10 +43,10 @@ class Store {
   /**
    * Создание записи
    */
-  createItem({code, title = 'Новая запись', selected = false}) {
+  createItem({code, title = 'Новая запись', selected = false, selectedCount = 0}) {
     this.setState({
       ...this.state,
-      items: this.state.items.concat({code, title, selected})
+      items: this.state.items.concat({code, title, selected, selectedCount})
     });
   }
 
@@ -69,11 +69,24 @@ class Store {
     this.setState({
       ...this.state,
       items: this.state.items.map(item => {
-        if (item.code === code){
-          item.selected = !item.selected;
+        if (item.code === code) {
+          item.selected =! item.selected;
+        } else {
+          item.selected = false
         }
         return item;
       })
+    });
+  }
+  selectedCount(code) {
+    this.setState({
+        ...this.state,
+        items: this.state.items.map(item => {
+          if (item.code === code) {
+            item.selected ? item.selectedCount++ : item.selectedCount
+          }
+          return item;
+        })
     });
   }
 }

5d5e3f9ff5a88e710b318b3376e69a74db2c58c8
Andrey <anyuvo@gmail.com>
2022-07-29 18:05:17 +0300

    use plural-ru npm

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {counter} from './utils.js';
 import './style.css';
+import plural from 'plural-ru'
 
 /**
  * Приложение
@@ -30,7 +31,7 @@ function App({store}) {
               <div className='Item__number'>{item.code}</div>
               <div className='Item__title'>
                 {item.title}
-                {!item.selectedCounter || ` | Выделялось ${item.selectedCounter} раз(а)`
+                {!item.selectedCounter || ` | Выделялось ${plural(item.selectedCounter,'%d раз', '%d раза', '%d раз')}`
                 }
               </div>
               <div className='Item__actions'>

90ddfbd13f9f909cb352f38eb1ff3ca798942f95
Dimahtml <bomabo@mail.ru>
2022-07-29 10:11:55 +0500

    lecture1. fix выбора элементов. счетчик выбора

--- src/app.js
+++ src/app.js
@@ -26,9 +26,17 @@ function App({store}) {
         <div className='List'>{items.map(item =>
           <div key={item.code} className='List__item'>
             <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-                 onClick={() => store.selectItem(item.code)}>
+                 onClick={
+                  () => {
+                    store.selectItem(item.code);
+                    store.clearRestItems(item.code);
+                    store.increaseSelectionsCount(item.code);
+                  }}>
               <div className='Item__number'>{item.code}</div>
               <div className='Item__title'>{item.title}</div>
+              {item.selectionsCount > 0 ?
+                <div className='Item__selections'>{`| Выделялся ${item.selectionsCount} раз`}</div> :
+                ''}
               <div className='Item__actions'>
                 <button onClick={() => store.deleteItem(item.code)}>
                   Удалить

4cded101b086c4f092a8bdb70238fbbd64349a93
korsakov <korsakov.ua.dn@gmail.com>
2022-07-29 12:44:15 +0300

    fix correct select of items, add info about the number of field selections

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {counter} from './utils.js';
 import './style.css';
 
@@ -16,19 +16,46 @@ function App({store}) {
       <div className='App__head'>
         <h1>Приложение на чистом JS</h1>
       </div>
+
       <div className='Controls'>
         <button onClick={() => {
           const code = counter();
           store.createItem({code, title: `Новая запись ${code}`})
         }}> Добавить </button>
       </div>
+
       <div className='App__center'>
-        <div className='List'>{items.map(item =>
-          <div key={item.code} className='List__item'>
+        <div className='List'>{items.map(item => <ListItem item={item} store={store} key={item.code} />)}</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+const ListItem = ({ item, store }) => {
+
+  const [count, setCount] = useState(0)
+
+  const countOfSelectText = count ? `| Выделился ${count} раз` : ''
+
+  const clickItemHandler = (e) => {
+    store.selectItem(item.code)
+
+    // получаю массив классов которые содержит текущий элемент
+    const classList = Object.values(e.currentTarget.classList);
+
+    if (!classList.includes('Item_selected')) {
+      setCount((prevState) => prevState += 1)
+    }
+  }
+ 
+  return (
+    <div key={item.code} className='List__item'>
             <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-                 onClick={() => store.selectItem(item.code)}>
+                 onClick={(e) => clickItemHandler(e)}>
               <div className='Item__number'>{item.code}</div>
-              <div className='Item__title'>{item.title}</div>
+              <div className='Item__title'>{item.title} {countOfSelectText}</div>
               <div className='Item__actions'>
                 <button onClick={() => store.deleteItem(item.code)}>
                   Удалить
@@ -36,11 +63,5 @@ function App({store}) {
               </div>
             </div>
           </div>
-        )}
-        </div>
-      </div>
-    </div>
-  );
+  )
 }
-
-export default App;

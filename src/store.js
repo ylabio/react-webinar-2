f74df77eca1fc27be6af5abf32850f32e056689a
Saveliy <svolskey2929@gmail.com>
2022-07-29 15:06:11 +0300

    Добавил счетчик выделений; При выделении одного пункта, другие сбрасываются

--- src/store.js
+++ src/store.js
@@ -43,10 +43,10 @@ class Store {
   /**
    * Создание записи
    */
-  createItem({code, title = 'Новая запись', selected = false}) {
+  createItem({code, title = 'Новая запись', selected = false, selectionsCount = 0}) {
     this.setState({
       ...this.state,
-      items: this.state.items.concat({code, title, selected})
+      items: this.state.items.concat({code, title, selected, selectionsCount})
     });
   }
 
@@ -69,8 +69,10 @@ class Store {
     this.setState({
       ...this.state,
       items: this.state.items.map(item => {
-        if (item.code === code){
+        if (item.code === code && item.selected !== true){
+          this.state.items.forEach(item => item.selected = false);
           item.selected = !item.selected;
+          item.selectionsCount++;
         }
         return item;
       })

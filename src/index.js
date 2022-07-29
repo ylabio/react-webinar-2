6084b1cd83e54e96cf30843410a19be532240d5f
alpknx <alpknx@gmail.com>
2022-07-29 16:28:36 +0300

    Added a selection counter,  when one item is selected, the selection of others is reset

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import {counter} from './utils.js';
 
 const store = new Store({
   items: [
-    {code: counter(), title: 'Название элемента'},
-    {code: counter(), title: 'Некий объект'},
-    {code: counter(), title: 'Заголовок'},
-    {code: counter(), title: 'Короткое название'},
-    {code: counter(), title: 'Запись'},
-    {code: counter(), title: 'Пример выделенной записи', selected: true},
-    {code: counter(), title: 'Седьмой'},
+	{code: counter(), title: 'Название элемента', selectionsCount: 0},
+	{code: counter(), title: 'Некий объект', selectionsCount: 0},
+	{code: counter(), title: 'Заголовок', selectionsCount: 0},
+	{code: counter(), title: 'Короткое название', selectionsCount: 0},
+	{code: counter(), title: 'Запись', selectionsCount: 0},
+	{code: counter(), title: 'Пример выделенной записи', selectionsCount: 0},
+	{code: counter(), title: 'Седьмой', selectionsCount: 0},
   ]
 });
 

fd992a81b6883ca3a501aed3d346d41575533cfd
vladimir <vladimirmuratov@mail.ru>
2022-07-29 14:00:22 +0300

    decision task1

--- src/store.js
+++ src/store.js
@@ -69,8 +69,10 @@ class Store {
     this.setState({
       ...this.state,
       items: this.state.items.map(item => {
+        item.selected = false
         if (item.code === code){
           item.selected = !item.selected;
+          item.countSelected = item.countSelected ? ++item.countSelected : 1
         }
         return item;
       })

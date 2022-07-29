ededc3ed122a422b9b5e2dcbce46ee60f77e50e8
ShizzaHo <chekan898@gmail.com>
2022-07-29 12:43:36 +0500

    Готовое задание

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import {counter} from './utils.js';
 
 const store = new Store({
   items: [
-    {code: counter(), title: 'Название элемента'},
-    {code: counter(), title: 'Некий объект'},
-    {code: counter(), title: 'Заголовок'},
-    {code: counter(), title: 'Короткое название'},
-    {code: counter(), title: 'Запись'},
-    {code: counter(), title: 'Пример выделенной записи', selected: true},
-    {code: counter(), title: 'Седьмой'},
+    {code: counter(), title: 'Название элемента', picksCount: 0},
+    {code: counter(), title: 'Некий объект', picksCount: 0},
+    {code: counter(), title: 'Заголовок', picksCount: 0},
+    {code: counter(), title: 'Короткое название', picksCount: 0},
+    {code: counter(), title: 'Запись', picksCount: 0},
+    {code: counter(), title: 'Пример выделенной записи', picksCount: 0, selected: true},
+    {code: counter(), title: 'Седьмой', picksCount: 0},
   ]
 });
 

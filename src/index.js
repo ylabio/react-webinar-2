35c3482b557d53ca58efbaaa93f397bc3ca71fde
Ninel-Zabaykina <zabaykinanm@mer.ci.nsu.ru>
2022-07-30 00:41:14 +0700

    second solution

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import {counter} from './utils.js';
 
 const store = new Store({
   items: [
-    {code: counter(), title: 'Название элемента'},
-    {code: counter(), title: 'Некий объект'},
-    {code: counter(), title: 'Заголовок'},
-    {code: counter(), title: 'Короткое название'},
-    {code: counter(), title: 'Запись'},
-    {code: counter(), title: 'Пример выделенной записи', selected: true},
-    {code: counter(), title: 'Седьмой'},
+    {code: counter(), title: 'Название элемента', selectionCount: 0},
+    {code: counter(), title: 'Некий объект', selectionCount: 0},
+    {code: counter(), title: 'Заголовок', selectionCount: 0},
+    {code: counter(), title: 'Короткое название', selectionCount: 0},
+    {code: counter(), title: 'Запись', selectionCount: 0},
+    {code: counter(), title: 'Пример выделенной записи', selected: true, selectionCount: 0},
+    {code: counter(), title: 'Седьмой', selectionCount: 0},
   ]
 });
 
--- src/store.js
+++ src/store.js
@@ -71,7 +71,9 @@ class Store {
       items: this.state.items.map(item => {
         if (item.code === code){
           item.selected = !item.selected;
-          item.selected && (item.selectionCount ? item.selectionCount++ : item.selectionCount = 1);
+          if (item.selected){
+            item.selectionCount++;
+          }
         } else {
           item.selected = false;
         }

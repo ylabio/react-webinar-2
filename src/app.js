d0cefce3f76a071aae8159c5c32529aad1148820
OBOLLL20333 <97986329+OBOLLL20333@users.noreply.github.com>
2022-07-30 20:48:30 +0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -8,41 +8,41 @@ import './style.css';
  * @return {React.ReactElement} Виртуальные элементы React
  */
 function App({ store }) {
-	// Выбор состояния из store
-	const { items } = store.getState();
+  // Выбор состояния из store
+  const { items } = store.getState();
 
-	return (
-		<div className='App'>
-			<div className='App__head'>
-				<h1>Приложение на чистом JS</h1>
-			</div>
-			<div className='Controls'>
-				<button onClick={() => {
-					const code = counter();
-					store.createItem({ code, title: `Новая запись ${code}` })
-				}}> Добавить </button>
-			</div>
-			<div className='App__center'>
-				<div className='List'>{items.map(item =>
-					<div key={item.code} className='List__item'>
-						<div className={'Item' + (item.selected ? ' Item_selected' : '')}
-							onClick={() => {
-								store.selectItem(item.code);
-							}}>
-							<div className='Item__number'>{item.code}</div>
-							<div className='Item__title'>{item.title + (item.count > 0 ? ` | Выделялся ${item.count} раз` : ``)}</div>
-							<div className='Item__actions'>
-								<button onClick={() => store.deleteItem(item.code)}>
-									Удалить
-								</button>
-							</div>
-						</div>
-					</div>
-				)}
-				</div>
-			</div>
-		</div >
-	);
+  return (
+    <div className='App'>
+      <div className='App__head'>
+        <h1>Приложение на чистом JS</h1>
+      </div>
+      <div className='Controls'>
+        <button onClick={() => {
+          const code = counter();
+          store.createItem({ code, title: `Новая запись ${code}` })
+        }}> Добавить </button>
+      </div>
+      <div className='App__center'>
+        <div className='List'>{items.map(item =>
+          <div key={item.code} className='List__item'>
+            <div className={'Item' + (item.selected ? ' Item_selected' : '')}
+              onClick={() => {
+                store.selectItem(item.code);
+              }}>
+              <div className='Item__number'>{item.code}</div>
+              <div className='Item__title'>{item.title + (item.count > 0 ? ` | Выделялся ${item.count}` : '') + (item.count > 10 && item.count < 20 ? `  раз` : item.count % 10 == 2 ? ' раза' : item.count % 10 == 3 ? ' раза' : item.count % 10 == 4 ? ' раза' : ' раз')}</div>
+              <div className='Item__actions'>
+                <button onClick={() => store.deleteItem(item.code)}>
+                  Удалить
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+        </div>
+      </div>
+    </div >
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ import Store from "./store.js";
 import { counter } from './utils.js';
 
 const store = new Store({
-	items: [
-		{ code: counter(), title: 'Название элемента', count: 0 },
-		{ code: counter(), title: 'Некий объект', count: 0 },
-		{ code: counter(), title: 'Заголовок', count: 0 },
-		{ code: counter(), title: 'Короткое название', count: 0 },
-		{ code: counter(), title: 'Запись', count: 0 },
-		{ code: counter(), title: 'Пример выделенной записи', selected: true, count: 0 },
-		{ code: counter(), title: 'Седьмой', count: 0 },
-	]
+  items: [
+    { code: counter(), title: 'Название элемента', count: 0 },
+    { code: counter(), title: 'Некий объект', count: 0 },
+    { code: counter(), title: 'Заголовок', count: 0 },
+    { code: counter(), title: 'Короткое название', count: 0 },
+    { code: counter(), title: 'Запись', count: 0 },
+    { code: counter(), title: 'Пример выделенной записи', selected: true, count: 1 },
+    { code: counter(), title: 'Седьмой', count: 0 },
+  ]
 });
 
 // Реакция на изменение store - повторный рендер приложения
 store.subscribe(() => {
-	ReactDOM.render(<App store={store} />, document.body);
+  ReactDOM.render(<App store={store} />, document.body);
 });
 
 // Первый рендер (один раз)

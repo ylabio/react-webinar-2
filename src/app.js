d4ac6aeedeee3371fc6ed63ce6b33e8d801c8b42
a0ne <mako666777@gmail.com>
2022-07-31 01:03:52 +0300

    added pluralize

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {counter} from './utils.js';
+import { pluralize } from "numeralize-ru"
 import './style.css';
 
 /**
@@ -28,7 +29,7 @@ function App({store}) {
             <div className={'Item' + (item.selected ? ' Item_selected' : '')}
                  onClick={() => store.selectItem(item.code)}>
               <div className='Item__number'>{item.code}</div>
-              <div className='Item__title'>{item.title} {item.count && ` | Выделялось ${item.count} раз(а)`}</div>
+              <div className='Item__title'>{item.title} {item.count && ` | Выделялось ${item.count}${pluralize(item.count, ' раз', ' раза', ' раз')} `}</div>
               <div className='Item__actions'>
                 <button onClick={() => store.deleteItem(item.code)}>
                   Удалить

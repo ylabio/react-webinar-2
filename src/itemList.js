afd1fa965d7d4ebf17483febd900a3ab49b51172
KirillDolyn <kirill.dolinskij@yandex.ru>
2022-07-30 15:04:44 +0300

    lecture1

---
+++ src/itemList.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function ItemList({ item, store }) {
+  const [state, setState] = React.useState(0);
+
+  const counter = () => setState(state + 1);
+
+  return (
+    <div key={item.code} className="List__item">
+      <div
+        className={"Item" + (item.selected ? " Item_selected" : "")}
+        onClick={() => store.selectItem(item.code)}
+      >
+        <div className="Item__number">{item.code}</div>
+        <div onClick={counter} className="Item__title">
+          {item.title} | Выделялось {state > 0 && <i>{state}</i>} раз
+        </div>
+        <div className="Item__actions">
+          <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ItemList;

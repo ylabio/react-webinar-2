d284ed36465b88251680b4b89937ba348a19abdc
Aleksandr Kucherenko <g28xyz@ya.ru>
2022-07-30 18:28:31 +0700

    feat: исправить ошибку в консоли ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app.js';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app.js";
 import Store from "./store.js";
-import {counter} from './utils.js';
+import { counter } from "./utils.js";
 
 const store = new Store({
   items: [
-    {code: counter(), title: 'Название элемента'},
-    {code: counter(), title: 'Некий объект'},
-    {code: counter(), title: 'Заголовок'},
-    {code: counter(), title: 'Короткое название'},
-    {code: counter(), title: 'Запись'},
-    {code: counter(), title: 'Пример выделенной записи', selected: true},
-    {code: counter(), title: 'Седьмой'},
-  ]
+    { code: counter(), title: "Название элемента" },
+    { code: counter(), title: "Некий объект" },
+    { code: counter(), title: "Заголовок" },
+    { code: counter(), title: "Короткое название" },
+    { code: counter(), title: "Запись" },
+    { code: counter(), title: "Пример выделенной записи", selected: true },
+    { code: counter(), title: "Седьмой" },
+  ],
 });
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // Реакция на изменение store - повторный рендер приложения
 store.subscribe(() => {
-  ReactDOM.render(<App store={store}/>, document.body);
+  root.render(<App store={store} />);
 });
 
 // Первый рендер (один раз)
-ReactDOM.render(<App store={store}/>, document.body);
+root.render(<App store={store} />);

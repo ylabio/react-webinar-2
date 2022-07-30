5f6e48cdc0f208797401e0a6c01afb3eef03eeb6
ASRync <m_igor_r@mail.ru>
2022-07-30 16:10:19 +0700

    create value counter

--- src/app.js
+++ src/app.js
@@ -29,6 +29,7 @@ function App({store}) {
                  onClick={() => store.selectItem(item.code)}>
               <div className='Item__number'>{item.code}</div>
               <div className='Item__title'>{item.title}</div>
+              {item > 0 && <div>Выделялось {item.value} раз</div> }
               <div className='Item__actions'>
                 <button onClick={() => store.deleteItem(item.code)}>
                   Удалить
--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import {counter} from './utils.js';
 
 const store = new Store({
   items: [
-    {code: counter(), title: 'Название элемента'},
-    {code: counter(), title: 'Некий объект'},
-    {code: counter(), title: 'Заголовок'},
-    {code: counter(), title: 'Короткое название'},
-    {code: counter(), title: 'Запись'},
-    {code: counter(), title: 'Пример выделенной записи', selected: true},
-    {code: counter(), title: 'Седьмой'},
+    {code: counter(), title: 'Название элемента', value: 0},
+    {code: counter(), title: 'Некий объект', value: 0},
+    {code: counter(), title: 'Заголовок', value: 0},
+    {code: counter(), title: 'Короткое название', value: 0},
+    {code: counter(), title: 'Запись', value: 0},
+    {code: counter(), title: 'Пример выделенной записи', selected: true, value: 0},
+    {code: counter(), title: 'Седьмой', value: 0},
   ]
 });
 

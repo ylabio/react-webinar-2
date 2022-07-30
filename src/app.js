81513ab351dad9eaa13c163d1b2910db70cdf220
244sinfall <dimafilin6@icloud.com>
2022-07-30 21:35:18 +0700

    lecture1 task. Fixed number plurs

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {counter} from './utils.js';
+import {counter, getNumberPluralized} from './utils.js';
 import './style.css';
 
 /**
@@ -28,7 +28,8 @@ function App({store}) {
             <div className={'Item' + (item.selected ? ' Item_selected' : '')}
                  onClick={() => store.selectItem(item.code)}>
               <div className='Item__number'>{item.code}</div>
-              <div className='Item__title'>{item.title}{item.selectedCount && ` | Выделялся ${item.selectedCount} раз(а)`}</div>
+              <div className='Item__title'>{item.title}{item.selectedCount && ` | Выделялся ${item.selectedCount}
+               ${getNumberPluralized(item.selectedCount, ["раз", "раза", "раз"])}`}</div>
               <div className='Item__actions'>
                 <button onClick={() => store.deleteItem(item.code)}>
                   Удалить

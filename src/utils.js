51b3e1f8872fde4eb8d85792603dd78605621e4a
Anton <tosha.kondratenko@yandex.ru>
2022-07-30 05:18:13 +0300

    добавил облегченную вариацию функций склонения слов

--- src/utils.js
+++ src/utils.js
@@ -6,11 +6,26 @@ export function counter() {
   return counter.value ? ++counter.value : counter.value = 1;
 }
 
+// Вариация №1 максимум условий
+// export function declensionWord(num) {
+
+//   const stringNum = String(num);
+//   if ((stringNum.includes('2') && stringNum.lastIndexOf('2') === stringNum.length - 1 && stringNum[stringNum.lastIndexOf('2') - 1] !== '1') ||
+//     (stringNum.includes('3') && stringNum.lastIndexOf('3') === stringNum.length - 1 && stringNum[stringNum.lastIndexOf('3') - 1] !== '1') ||
+//     (stringNum.includes('4') && stringNum.lastIndexOf('4') === stringNum.length - 1 && stringNum[stringNum.lastIndexOf('4') - 1] !== '1')) return 'раза';
+//   else return 'раз';
+// }
+
+// Вариация №2 упрощенная объектами
 export function declensionWord(num) {
 
-  const stringNum = String(num);
-  if ((stringNum.includes('2') && stringNum.lastIndexOf('2') === stringNum.length - 1 && stringNum[stringNum.lastIndexOf('2') - 1] !== '1') ||
-    (stringNum.includes('3') && stringNum.lastIndexOf('3') === stringNum.length - 1 && stringNum[stringNum.lastIndexOf('3') - 1] !== '1') ||
-    (stringNum.includes('4') && stringNum.lastIndexOf('4') === stringNum.length - 1 && stringNum[stringNum.lastIndexOf('4') - 1] !== '1')) return 'раза';
-  else return 'раз';
+  let stringNum = String(num);
+
+  if (stringNum.length >= 2 && stringNum[stringNum.length - 2] === '1') return "раз"
+
+  if (stringNum.length >= 2) stringNum = stringNum[stringNum.length - 1];
+
+  return { 2: 'раза', 3: 'раза', 4: 'раза' }[stringNum] || { 0: 'раз', 1: 'раз', 5: 'раз', 6: 'раз', 7: 'раз', 8: 'раз', 9: 'раз' }[stringNum]
 }
+
+

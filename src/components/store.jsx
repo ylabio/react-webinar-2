aabeb98534323b6de017c6c4f7b45e73164d763f
Konstantin Z <kostjakot31@gmail.com>
2022-07-30 23:37:22 +0300

    lecture1

---
+++ src/components/store.jsx
@@ -0,0 +1,84 @@
+class Store {
+	constructor(initState) {
+		// Состояние приложения (данные)
+		this.state = initState
+		// Слушатели изменений state
+		this.listeners = []
+	}
+
+	/**
+	 * Выбор state
+	 * @return {Object}
+	 */
+	getState() {
+		return this.state
+	}
+
+	/**
+	 * Установка state
+	 * @param newState {Object}
+	 */
+	setState(newState) {
+		this.state = newState
+		// Оповещаем всех подписчиков об изменении стейта
+		for (const lister of this.listeners) {
+			lister()
+		}
+	}
+
+	/**
+	 * Подписка на изменение state
+	 * @param callback {Function}
+	 * @return {Function} Функция для отписки
+	 */
+	subscribe(callback) {
+		this.listeners.push(callback)
+		// Возвращаем функцию для удаления слушателя
+		return () => {
+			this.listeners = this.listeners.filter(item => item !== callback)
+		}
+	}
+
+	/**
+	 * Создание записи
+	 */
+	createItem({ code, title = 'Новая запись', selected = false }) {
+		this.setState({
+			...this.state,
+			items: this.state.items.concat({
+				code,
+				title,
+				selected,
+			}),
+		})
+	}
+
+	/**
+	 * Удаление записи по её коду
+	 * @param code
+	 */
+	deleteItem(code) {
+		this.setState({
+			...this.state,
+			items: this.state.items.filter(item => item.code !== code),
+		})
+	}
+
+	/**
+	 * Выделение записи по её коду
+	 * @param code
+	 */
+	selectItem(code) {
+		this.setState({
+			...this.state,
+			items: this.state.items.map(item => {
+				if (item.code === code) {
+					item.selected = true
+				} else item.selected = false
+				return item
+			}),
+		})
+	}
+}
+
+export default Store

54bf809709231b75c150372f7788c6699fdef072
Dubinin Anton <birthday2712@gmail.com>
2022-07-30 12:36:10 +0500

    row selection and selection counting

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import {counter} from './utils.js';
 
 const store = new Store({
   items: [
-    {code: counter(), title: 'Название элемента'},
-    {code: counter(), title: 'Некий объект'},
-    {code: counter(), title: 'Заголовок'},
-    {code: counter(), title: 'Короткое название'},
-    {code: counter(), title: 'Запись'},
-    {code: counter(), title: 'Пример выделенной записи', selected: true},
-    {code: counter(), title: 'Седьмой'},
+    {code: counter(), title: 'Название элемента', count: false}, /* Добавил count в каждый элемент */
+    {code: counter(), title: 'Некий объект', count: false},
+    {code: counter(), title: 'Заголовок', count: false},
+    {code: counter(), title: 'Короткое название', count: false},
+    {code: counter(), title: 'Запись', count: false},
+    {code: counter(), title: 'Пример выделенной записи', selected: true, count: false},
+    {code: counter(), title: 'Седьмой', count: false},
   ]
 });
 
--- src/store.js
+++ src/store.js
@@ -43,10 +43,11 @@ class Store {
   /**
    * Создание записи
    */
-  createItem({code, title = 'Новая запись', selected = false}) {
+  /* Добавил count */
+  createItem({code, title = 'Новая запись', selected = false, count = 0}) { 
     this.setState({
       ...this.state,
-      items: this.state.items.concat({code, title, selected})
+      items: this.state.items.concat({code, title, selected, count})
     });
   }
 
@@ -71,6 +72,11 @@ class Store {
       items: this.state.items.map(item => {
         if (item.code === code){
           item.selected = !item.selected;
+          if(item.selected) {
+            item.count ++;
+          } /* Счетчик количества раз выделения пунктов меню */
+        } else {
+          item.selected = false /* Условие для реализации задания по сбросу выделения у всех пунктов меню кроме выбранного */
         }
         return item;
       })

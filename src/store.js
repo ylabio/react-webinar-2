4f241baf0c57391f39a43f36b6a8b5cf61d4ef20
Ivan <duremar98@yandex.ru>
2022-07-31 15:11:58 +0300

    +

--- src/store.js
+++ src/store.js
@@ -1,5 +1,4 @@
 class Store {
-
   constructor(initState) {
     // Состояние приложения (данные)
     this.state = initState;
@@ -36,17 +35,17 @@ class Store {
     this.listners.push(callback);
     // Возвращаем функцию для удаления слушателя
     return () => {
-      this.listners = this.listners.filter(item => item !== callback);
-    }
+      this.listners = this.listners.filter((item) => item !== callback);
+    };
   }
 
   /**
    * Создание записи
    */
-  createItem({code, title = 'Новая запись', selected = false}) {
+  createItem({ code, title = "Новая запись", selected = false }) {
     this.setState({
       ...this.state,
-      items: this.state.items.concat({code, title, selected})
+      items: this.state.items.concat({ code, title, selected }),
     });
   }
 
@@ -57,7 +56,7 @@ class Store {
   deleteItem(code) {
     this.setState({
       ...this.state,
-      items: this.state.items.filter(item => item.code !== code)
+      items: this.state.items.filter((item) => item.code !== code),
     });
   }
 
@@ -68,12 +67,19 @@ class Store {
   selectItem(code) {
     this.setState({
       ...this.state,
-      items: this.state.items.map(item => {
-        if (item.code === code){
+      items: this.state.items.map((item) => {
+        if (item.code === code) {
           item.selected = !item.selected;
+          if (item.selected) {
+            !item.counter ? (item.counter = 1) : item.counter++;
+          }
         }
+        if (item.code !== code) {
+          item.selected = false;
+        }
+
         return item;
-      })
+      }),
     });
   }
 }

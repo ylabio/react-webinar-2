4852dfd105610f8b2186991fd069f4c22dc9baf6
psuzanna <levonliamik@mail.ru>
2022-07-31 09:06:23 +0400

    lec1

--- src/store.js
+++ src/store.js
@@ -43,10 +43,10 @@ class Store {
   /**
    * Создание записи
    */
-  createItem({code, title = 'Новая запись', selected = false}) {
+  createItem({ code, title = 'Новая запись', selected = false, clickCounter = 0 }) {
     this.setState({
       ...this.state,
-      items: this.state.items.concat({code, title, selected})
+      items: this.state.items.concat({ code, title, selected, clickCounter })
     });
   }
 
@@ -69,9 +69,15 @@ class Store {
     this.setState({
       ...this.state,
       items: this.state.items.map(item => {
-        if (item.code === code){
+        if (item.code === code) {
           item.selected = !item.selected;
+          if (item.selected) item.clickCounter++;
         }
+
+        else {
+          item.selected = false;
+        }
+
         return item;
       })
     });

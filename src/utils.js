58973a55cbfd058ead5a5c9c5b915a917465104b
CraigBit <listokbymagi@gmail.com>
2022-08-02 06:46:23 +0700

    pluralizer fixed, switchted to the one from npm

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+const plural = require('plural-ru');
+
 /**
  * Генерирует уникальный код на основе счётчика
  * @returns {number|number}
@@ -7,36 +9,5 @@ export function counter() {
 }
 
 export function wordEnding(int) {
-  // превращаем входное число в строку и разбиваем строку на массив.
-
-  let intToArray = String(int).split('');
-
-  // получаем последний элемент массива
-
-  let lastChar = intToArray[intToArray.length - 1];
-
-  // получаем предпоследний элемент массива
-
-  let preLastChar = intToArray[intToArray.length - 2];
-
-  // делаем проверки
-  // для 0
-
-  if (lastChar == 0) return `${int} раз`;
-
-  // для чисел, у которых последний знак будет от 5 до 9 включительно
-
-  if (lastChar >= 5 && lastChar <= 9) return `${int} раз`;
-
-  // для чисел 11, 12, 13, 14
-
-  if (preLastChar == 1 && lastChar >= 1 && lastChar <= 4) return `${int} раз`;
-
-  // для чисел 1, 21, 31, 41 и т.д.
-
-  if (preLastChar != 1 && lastChar == 1) return `${int} раз`;
-
-  // для чисел 2, 3, 4, 122, 323, 724 и т.д.
-
-  if (preLastChar != 1 && lastChar >= 2 && lastChar <= 4) return `${int} раза`;
+  return plural(int, '%d раз', '%d раза', '%d раз');
 }

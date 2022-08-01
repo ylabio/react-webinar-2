d0201a1e48243c14b0031dca8d9f9ae922d1d7fb
Андрей <moon1ight2013@yandex.ru>
2022-08-02 01:07:54 +0300

    Чуть улучшена функция showtimes

--- src/utils.js
+++ src/utils.js
@@ -7,11 +7,10 @@ export function counter() {
 }
 
 export function showTimes(number) {
-  const remainder  = number % 10;
   let word = 'раз';
   if (number >= 11 && number <= 21) {
     word = 'раз'
-  } else if (remainder  >= 2 && remainder  <= 4) {
+  } else if (number % 10 >= 2 && number % 10 <= 4) {
     word = 'раза'
   }
   return `${number + ' ' + word}`;

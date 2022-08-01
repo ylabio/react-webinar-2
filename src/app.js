4c3d09c2ca05ed2e6ea1410883942332982a7759
llss1989 <argenkasymov2@gmail.com>
2022-08-01 23:27:37 +0300

    пофиксил слово раз

--- src/app.js
+++ src/app.js
@@ -11,8 +11,17 @@ function App({store}) {
   // Выбор состояния из store
   const {items} = store.getState();
   
+  const returnRazWord = (num) => {
+    if (num === 12 || num === 13 || num === 14) {
+      return 'раз'
+    }
+    const str = num.toString();
+    const reg = /[2,3,4]$/gm;
+    return reg.test(str) ? 'раза' : 'раз'
+  }
+
   const buildItem = ({selected, code, title, countSelected}) => {
-    const countSelectedHtmlWithLine = <div className='Item__info-inner-box'><div className='Item__vertical-line'></div><p className='Item__countSelected'>{`Выделялся ${countSelected}`}</p></div>;
+    const countSelectedHtmlWithLine = <div className='Item__info-inner-box'><div className='Item__vertical-line'></div><p className='Item__countSelected'>{`Выделялся ${countSelected} ${returnRazWord(countSelected)}`}</p></div>;
     return <div key={code} className='List__item'>
         <div className={'Item' + (selected ? ' Item_selected' : '')}
             onClick={() => {
@@ -21,7 +30,6 @@ function App({store}) {
           <div className='Item__number'>{code}</div>
           <div className="Item__info-box">
             <div className='Item__title'>{title}</div>
-            
             {countSelected > 0 && countSelectedHtmlWithLine}
           </div>
           

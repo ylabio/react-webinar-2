2ec05f48f34ed3c9a0de1cb82c58839727f94a9c
Vladon305 <shuhow003@mail.ru>
2022-08-01 11:57:00 +0300

    Add functionality

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import {counter} from './utils.js';
-import './style.css';
+import React from 'react'
+import { counter } from './utils.js'
+import './style.css'
 
 /**
  * Приложение
  * @param store {Store} Состояние приложения
  * @return {React.ReactElement} Виртуальные элементы React
  */
-function App({store}) {
+function App({ store }) {
   // Выбор состояния из store
-  const {items} = store.getState();
+  const { items } = store.getState()
 
   return (
     <div className='App'>
@@ -17,30 +17,41 @@ function App({store}) {
         <h1>Приложение на чистом JS</h1>
       </div>
       <div className='Controls'>
-        <button onClick={() => {
-          const code = counter();
-          store.createItem({code, title: `Новая запись ${code}`})
-        }}> Добавить </button>
+        <button
+          onClick={() => {
+            const code = counter()
+            store.createItem({ code, title: `Новая запись ${code}` })
+          }}
+        >
+          Добавить
+        </button>
       </div>
       <div className='App__center'>
-        <div className='List'>{items.map(item =>
-          <div key={item.code} className='List__item'>
-            <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-                 onClick={() => store.selectItem(item.code)}>
-              <div className='Item__number'>{item.code}</div>
-              <div className='Item__title'>{item.title}</div>
-              <div className='Item__actions'>
-                <button onClick={() => store.deleteItem(item.code)}>
-                  Удалить
-                </button>
+        <div className='List'>
+          {items.map((item) => (
+            <div key={item.code} className='List__item'>
+              <div
+                className={'Item' + (item.selected ? ' Item_selected' : '')}
+                onClick={() => store.selectItem(item.code)}
+              >
+                <div className='Item__number'>{item.code}</div>
+                <div className='Item__title'>
+                  {item.title}
+                  {item.selectCounter > 0 &&
+                    ` | Выделялся ${item.selectCounter} раз`}
+                </div>
+                <div className='Item__actions'>
+                  <button onClick={() => store.deleteItem(item.code)}>
+                    Удалить
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        )}
+          ))}
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/store.js
+++ src/store.js
@@ -1,10 +1,9 @@
 class Store {
-
   constructor(initState) {
     // Состояние приложения (данные)
-    this.state = initState;
+    this.state = initState
     // Слушатели изменений state
-    this.listners = [];
+    this.listners = []
   }
 
   /**
@@ -12,7 +11,7 @@ class Store {
    * @return {Object}
    */
   getState() {
-    return this.state;
+    return this.state
   }
 
   /**
@@ -20,10 +19,10 @@ class Store {
    * @param newState {Object}
    */
   setState(newState) {
-    this.state = newState;
+    this.state = newState
     // Оповещаем всех подписчиков об изменении стейта
     for (const lister of this.listners) {
-      lister();
+      lister()
     }
   }
 
@@ -33,21 +32,26 @@ class Store {
    * @return {Function} Функция для отписки
    */
   subscribe(callback) {
-    this.listners.push(callback);
+    this.listners.push(callback)
     // Возвращаем функцию для удаления слушателя
     return () => {
-      this.listners = this.listners.filter(item => item !== callback);
+      this.listners = this.listners.filter((item) => item !== callback)
     }
   }
 
   /**
    * Создание записи
    */
-  createItem({code, title = 'Новая запись', selected = false}) {
+  createItem({
+    code,
+    title = 'Новая запись',
+    selected = false,
+    selectCounter = 0,
+  }) {
     this.setState({
       ...this.state,
-      items: this.state.items.concat({code, title, selected})
-    });
+      items: this.state.items.concat({ code, title, selected, selectCounter }),
+    })
   }
 
   /**
@@ -57,8 +61,8 @@ class Store {
   deleteItem(code) {
     this.setState({
       ...this.state,
-      items: this.state.items.filter(item => item.code !== code)
-    });
+      items: this.state.items.filter((item) => item.code !== code),
+    })
   }
 
   /**
@@ -68,14 +72,18 @@ class Store {
   selectItem(code) {
     this.setState({
       ...this.state,
-      items: this.state.items.map(item => {
-        if (item.code === code){
-          item.selected = !item.selected;
+      items: this.state.items.map((item) => {
+        if (item.code === code) {
+          item.selected = !item.selected
+          item.selected && ++item.selectCounter
         }
-        return item;
-      })
-    });
+        if (item.code !== code) {
+          item.selected = false
+        }
+        return item
+      }),
+    })
   }
 }
 
-export default Store;
+export default Store

978b8cacc82b5673a801049e7ce6d836d20d6bea
Андрей <moon1ight2013@yandex.ru>
2022-08-02 00:36:36 +0300

    Написана функция для склонения раз, при удалении не сбрасывается выделение

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { counter } from './utils.js';
+import { counter, showTimes } from './utils.js';
 import './style.css';
 
 /**
@@ -29,20 +29,24 @@ function App({ store }) {
             <div className={'Item' + (item.selected ? ' Item_selected' : '')}
               onClick={() => store.selectItem(item.code)}>
               <div className='Item__number'>{item.code}</div>
-              <div className='Item__title'>{item.title}{item.count > 0 ? ` | Выделялось ${item.count} раз` : null}
+              <div className='Item__title'>{item.title}{item.count > 0 ? ` | Выделялось ${showTimes(item.count)}` : null}
               </div>
               <div className='Item__actions'>
-                <button onClick={() => store.deleteItem(item.code)}>
-                  Удалить
-                </button>
-              </div>
+                <button onClick={(e) =>{
+                  e.stopPropagation()
+                  store.deleteItem(item.code)
+                }
+                  }>
+                Удалить
+              </button>
             </div>
+          </div>
 
           </div>
         )}
-        </div>
       </div>
     </div>
+    </div >
   );
 }
 
--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,18 @@
  * Генерирует уникальный код на основе счётчика
  * @returns {number|number}
  */
-export function counter(){
+export function counter() {
   return counter.value ? ++counter.value : counter.value = 1;
 }
+
+export function showTimes(number) {
+  const remainder  = number % 10;
+  let word = 'раз';
+  if (number >= 11 && number <= 21) {
+    word = 'раз'
+  } else if (remainder  >= 2 && remainder  <= 4) {
+    word = 'раза'
+  }
+  return `${number + ' ' + word}`;
+}
+

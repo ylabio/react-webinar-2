a6e02e2f4cd5e1851fd7b68422a58fc5e7c6cea6
maxtereshchenko <aralon2843@gmail.com>
2022-08-01 21:19:27 +0300

    fix: changed select behavior after deleting some item

--- src/app.js
+++ src/app.js
@@ -48,7 +48,12 @@ function App({ store }) {
                 </div>
 
                 <div className="Item__actions">
-                  <button onClick={() => store.deleteItem(item.code)}>
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      store.deleteItem(item.code);
+                    }}
+                  >
                     Удалить
                   </button>
                 </div>

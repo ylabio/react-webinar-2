75430f2414c5670c24f5f5dc9e3c7ff2055ffac5
Shakhrai-Antony <manager.laune@gmail.com>
2022-08-02 20:35:47 +0300

    added plural-ru

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {counter} from './utils.js';
 import './style.css';
+import plural from 'plural-ru'
 
 /**
  * Приложение
@@ -28,7 +29,7 @@ function App({store}) {
             <div className={'Item' + (item.selected ? ' Item_selected' : '')}
                  onClick={() => store.selectItem(item.code)}>
               <div className='Item__number'>{item.code}</div>
-              <div className='Item__title'>{item.title} {item.count > 0 ? `| Выделялось ${item.count} раз(а)` : ''}</div>
+              <div className='Item__title'>{item.title} {item.count > 0 ? `| Выделялось ${plural(item.count, ' %d раз', ' %d раза', ' %d раз')}` : ''}</div>
               <div className='Item__actions'>
                 <button onClick={() => store.deleteItem(item.code)}>
                   Удалить

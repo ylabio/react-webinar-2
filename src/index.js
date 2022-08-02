6b21b2003667c21db434da29b542f28db6182656
Katerina <egerc28@gmail.com>
2022-08-02 09:22:50 +0300

    homework is done

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app.js";
 import Store from "./store.js";
-import {counter} from './utils.js';
+import { counter } from "./utils.js";
 
 const store = new Store({
   items: [
-    {code: counter(), title: 'Название элемента'},
-    {code: counter(), title: 'Некий объект'},
-    {code: counter(), title: 'Заголовок'},
-    {code: counter(), title: 'Короткое название'},
-    {code: counter(), title: 'Запись'},
-    {code: counter(), title: 'Пример выделенной записи', selected: true},
-    {code: counter(), title: 'Седьмой'},
-  ]
+    { code: counter(), title: "Название элемента", total: 0 },
+    { code: counter(), title: "Некий объект", total: 0 },
+    { code: counter(), title: "Заголовок", total: 0 },
+    { code: counter(), title: "Короткое название", total: 0 },
+    { code: counter(), title: "Запись", total: 0 },
+    {
+      code: counter(),
+      title: "Пример выделенной записи",
+      selected: true,
+      total: 0,
+    },
+    { code: counter(), title: "Седьмой", total: 0 },
+  ],
 });
 
 // Реакция на изменение store - повторный рендер приложения
 store.subscribe(() => {
-  ReactDOM.render(<App store={store}/>, document.body);
+  ReactDOM.render(<App store={store} />, document.body);
 });
 
 // Первый рендер (один раз)
-ReactDOM.render(<App store={store}/>, document.body);
+ReactDOM.render(<App store={store} />, document.body);
--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,24 @@
  * Генерирует уникальный код на основе счётчика
  * @returns {number|number}
  */
-export function counter(){
-  return counter.value ? ++counter.value : counter.value = 1;
+export function counter() {
+  return counter.value ? ++counter.value : (counter.value = 1);
+}
+
+/**
+ * Генерирует склонение слов: раз/раза
+ */
+
+export function declOfNumber(number) {
+  number = Math.abs(number) % 100;
+  const number1 = number % 10;
+  if (number > 10 && number < 20) {
+    return "раз";
+  } else if (number1 > 1 && number1 < 5) {
+    return "раза";
+  } else if (number1 == 1) {
+    return "раз";
+  } else {
+    return "раз";
+  }
 }

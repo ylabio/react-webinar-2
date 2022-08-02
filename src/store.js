1553e08d559710fb4bf70f636a9f9df199747f04
Youra <yurykishko@yandex.ru>
2022-08-02 19:32:00 +0800

    0000-ylab-lecture1

--- src/store.js
+++ src/store.js
@@ -43,10 +43,10 @@ class Store {
   /**
    * Создание записи
    */
-  createItem({code, title = 'Новая запись', selected = false}) {
+  createItem({code, title = 'Новая запись', selected = false, count = 0}) {
     this.setState({
       ...this.state,
-      items: this.state.items.concat({code, title, selected})
+      items: this.state.items.concat({code, title, selected, count})
     });
   }
 
@@ -69,8 +69,15 @@ class Store {
     this.setState({
       ...this.state,
       items: this.state.items.map(item => {
-        if (item.code === code){
+        if (item.code !== code) {
+          item.selected = false;
+        }
+
+        if (item.code === code) {
           item.selected = !item.selected;
+          if (item.selected === true) {
+            item.count += 1;
+          }
         }
         return item;
       })

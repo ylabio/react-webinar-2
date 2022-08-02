4bccb54e43493f2db379e1df52b64cee1ae23152
Victoria Baeva <33494058+VictoriaBaeva@users.noreply.github.com>
2022-08-02 13:18:29 +0300

    Вывод 'раз' или 'раза'
    Исправила вывод 'раз' или 'раза' в зависимости от числа с помощью регулярного выражения.

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ function App({store}) {
             <div className={'Item' + (item.selected ? ' Item_selected' : '')}
                  onClick={() => store.selectItem(item.code)}>
               <div className='Item__number'>{item.code}</div>
-              <div className='Item__title'>{item.title}{item.countSelected === 0 ? '' :` | Выделялся: ${item.countSelected} раз`}</div>
+              <div className='Item__title'>{item.title}{item.countSelected === 0 ? '' :` | Выделялся: ${item.countSelected}${String(item.countSelected).match(/[234]$/) ? ' раза' : ' раз'}`}</div>
               <div className='Item__actions'>
                 <button onClick={() => store.deleteItem(item.code)}>
                   Удалить

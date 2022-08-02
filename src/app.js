ce6a924c95846a1447202cc78035d0daa6529a00
Evgeny Strelnikov <estrelnikov1@yandex.ru>
2022-08-02 19:48:40 +0300

    update lecture1

--- src/app.js
+++ src/app.js
@@ -1,16 +1,7 @@
 import React from "react";
-import { counter } from "./utils.js";
+import { counter, renderPhrase } from "./utils.js";
 import "./style.css";
 
-const renderPhrase = (number) => {
-  const lastOne = Number(number.toString().slice(-1));
-  if (number > 4 && number < 15) {
-    return `Выделялось ${number} раз`;
-  }
-  if (lastOne === 1) return `Выделялось ${number} раз`;
-  if ([2, 3, 4].indexOf(lastOne) >= 0) return `Выделялось ${number} раза`;
-  return `Выделялось ${number} раз`;
-};
 /**
  * Приложение
  * @param store {Store} Состояние приложения
--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,16 @@
  * Генерирует уникальный код на основе счётчика
  * @returns {number|number}
  */
-export function counter(){
-  return counter.value ? ++counter.value : counter.value = 1;
+export function counter() {
+  return counter.value ? ++counter.value : (counter.value = 1);
 }
+
+export const renderPhrase = (number) => {
+  const lastOne = Number(number.toString().slice(-1));
+  if (number > 4 && number < 15) {
+    return `Выделялось ${number} раз`;
+  }
+  if (lastOne === 1) return `Выделялось ${number} раз`;
+  if ([2, 3, 4].indexOf(lastOne) >= 0) return `Выделялось ${number} раза`;
+  return `Выделялось ${number} раз`;
+};

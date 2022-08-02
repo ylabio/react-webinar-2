9f8701e99c9c2b8d401b587263bb12334d0b354c
Aleksandr Bolokhov <bolohov@inbox.ru>
2022-08-02 19:34:34 +0300

    corrected_homework_1_add_pluralism

--- src/utils.js
+++ src/utils.js
@@ -2,12 +2,17 @@
  * Генерирует уникальный код на основе счётчика
  * @returns {number|number}
  */
+
 export function counter(){
   return counter.value ? ++counter.value : counter.value = 1;
 }
 
 export function renderCountSelection (item) {
   if(item.countSelected > 0)  {
-    return  ' | Выделялось ' + item.countSelected + ' раз';
+    if(item.countSelected % 10 > 1 && item.countSelected % 10 < 5 && item.countSelected !=12 && item.countSelected !=13 && item.countSelected !=14) {
+    return  ' | Выделялось ' + item.countSelected + ' раза';
+    } else {
+      return  ' | Выделялось ' + item.countSelected + ' раз';
+    };
   };
  };

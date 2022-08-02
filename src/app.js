8c5dd12958faec7d80752ada2c1042a0a777f682
konstantin.korovin <cosmo.stan@yandex.com>
2022-08-02 23:57:35 +0300

    fix the output of the number taking into account the plural

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
 import {counter} from './utils.js';
 import './style.css';
 
@@ -7,7 +9,27 @@ import './style.css';
  * @param store {Store} Состояние приложения
  * @return {React.ReactElement} Виртуальные элементы React
  */
+
+i18n.use(initReactI18next).init({
+  resources: {
+    ru: {
+      translation: {
+        select_zero: ' | Выделялось {{count}} раз',
+        select_one: ' | Выделялось {{count}} раз',
+        select_few: ' | Выделялось {{count}} раза',
+        select_many: ' | Выделялось {{count}} раз',
+      }
+    }
+  },
+  lng: 'ru',
+  fallbackLng: 'ru',
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
 function App({store}) {
+  const { t } = useTranslation();
   // Выбор состояния из store
   const {items} = store.getState();
 
@@ -30,7 +52,7 @@ function App({store}) {
               <div className='Item__number'>{item.code}</div>
               <div className='Item__title'>
                 {item.title}
-                {item.selectCount > 0 ? ` | Выделялось ${item.selectCount} раз` : null}
+                {item.selectCount > 0 ? t('select', { count: item.selectCount }) : null}
               </div>
               <div className='Item__actions'>
                 <button onClick={() => store.deleteItem(item.code)}>

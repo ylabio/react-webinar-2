4a6f842dba54d22de3ee910860c46dff916ce057
Dima12411 <fixer40delta@gmail.com>
2022-08-02 15:01:49 +0300

    fixed

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import getNoun from 'plural-ru';
 import { counter } from './utils.js';
 import './style.css';
 
@@ -43,16 +44,16 @@ function App( { store } ) {
                 <div className='List'>{ items.map(item =>
                     <div key={ item.code } className='List__item'>
                         <div className={ 'Item' + ( item.selected ? ' Item_selected' : '' ) }
-                             onClick={ () => store.selectItem(item.code) || store.selectQuantity(item.code) }>
+                             onClick={ () => store.selectItem(item.code) }>
                             <div className='Item__number'>{ item.code }</div>
                             <div
                                 className='Item__title'>{ item.title }
                                 { item.selectedCount
-                                    ? ` | Выделялось ${ item.selectedCount } ${ getNoun(item.selectedCount) }`
+                                    ? ` | Выделялось ${ item.selectedCount } ${ getNoun(item.selectedCount, 'раз', 'раза', 'раз') }`
                                     : '' }
                             </div>
                             <div className='Item__actions'>
-                                <button onClick={ ( e ) => e.stopPropagation(store.deleteItem(item.code)) }>
+                                <button onClick={ ( e ) => store.deleteItem(item.code, e) }>
                                     Удалить
                                 </button>
                             </div>

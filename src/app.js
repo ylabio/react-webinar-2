0c218391b7f1839a5724f43e6758679465b96bc7
ILia_Churilov <churillov@mail.ru>
2022-08-02 21:39:57 +0700

    1) Доработал выделение пунктов, чтобы при выделении сбрасывалось выделения у всех других пунктов. А именно доработал условие в selectItem и изменил действия при выполнении разных условий.
    2) Вывел количество совершенных выделений для каждого пункта фразой “Выделялось N раз”. Добавил переменную index по умолчанию равную 0. В методе selectItem, инкрементируем переменную index при клике на строку.  Изменил в файле app.js вывод title.

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ function App({store}) {
             <div className={'Item' + (item.selected ? ' Item_selected' : '')}
                  onClick={() => store.selectItem(item.code)}>
               <div className='Item__number'>{item.code}</div>
-              <div className='Item__title'>{item.title}</div>
+              <div className='Item__title'>{(item.index === 0) ? item.title : `${item.title} | Выделялось ${item.index} раз`}</div>
               <div className='Item__actions'>
                 <button onClick={() => store.deleteItem(item.code)}>
                   Удалить
--- src/store.js
+++ src/store.js
@@ -43,10 +43,10 @@ class Store {
   /**
    * Создание записи
    */
-  createItem({code, title = 'Новая запись', selected = false}) {
+  createItem({code, title = 'Новая запись', index = 0, selected = false}) {
     this.setState({
       ...this.state,
-      items: this.state.items.concat({code, title, selected})
+      items: this.state.items.concat({code, title, index, selected})
     });
   }
 
@@ -70,7 +70,10 @@ class Store {
       ...this.state,
       items: this.state.items.map(item => {
         if (item.code === code){
-          item.selected = !item.selected;
+          item.selected = true;
+          item.index += 1
+        } else {
+          item.selected = false
         }
         return item;
       })

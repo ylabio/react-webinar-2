12ccf475fd6d9b338a57a18b3fa3f87601d007cf
AlekseyGitPub <62948770+AlekseyGitPub@users.noreply.github.com>
2022-08-03 16:06:36 +0300

    Move function

--- src/app.js
+++ src/app.js
@@ -43,9 +43,4 @@ function App({store}) {
   );
 }
 
-function declOfNum(number, titles) {  
-  const cases = [2, 0, 1, 1, 1, 2];  
-  return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];  
-}
-
 export default App;

2362092643df0162fae76d0479e5444179a5cc23
unatt <yurinnik@gmail.com>
2022-08-03 08:59:02 +0300

    fixed bugs, added 'раза-раз' case

--- src/app.js
+++ src/app.js
@@ -8,51 +8,70 @@ import './style.css';
  * @return {React.ReactElement} Виртуальные элементы React
  */
 function App({ store }) {
-  // Выбор состояния из store
-  const { items } = store.getState();
+    // Выбор состояния из store
+    const { items } = store.getState();
 
-  return (
-    <div className='App'>
-      <div className='App__head'>
-        <h1>Приложение на чистом JS</h1>
-      </div>
-      <div className='Controls'>
-        <button
-          onClick={() => {
-            const code = counter();
-            store.createItem({ code, title: `Новая запись ${code}` });
-          }}
-        >
-          {' '}
-          Добавить{' '}
-        </button>
-      </div>
-      <div className='App__center'>
-        <div className='List'>
-          {items.map((item) => (
-            <div key={item.code} className='List__item'>
-              <div
-                className={'Item' + (item.selected ? ' Item_selected' : '')}
-                onClick={() => store.selectItem(item.code)}
-              >
-                <div className='Item__number'>{item.code}</div>
-                <div className='Item__title'>
-                  {item.title}{' '}
-                  {item.timesSelected > 0 &&
-                    `| Выделялся ${item.timesSelected} раз`}
-                </div>
-                <div className='Item__actions'>
-                  <button onClick={() => store.deleteItem(item.code)}>
-                    Удалить
-                  </button>
-                </div>
-              </div>
+    return (
+        <div className="App">
+            <div className="App__head">
+                <h1>Приложение на чистом JS</h1>
             </div>
-          ))}
+            <div className="Controls">
+                <button
+                    onClick={() => {
+                        const code = counter();
+                        store.createItem({
+                            code,
+                            title: `Новая запись ${code}`,
+                        });
+                    }}
+                >
+                    {' '}
+                    Добавить{' '}
+                </button>
+            </div>
+            <div className="App__center">
+                <div className="List">
+                    {items.map((item) => {
+                        const selectedTimesContent =
+                            item.timesSelected > 1 && item.timesSelected < 5
+                                ? `| Выделялся ${item.timesSelected} разa`
+                                : `| Выделялся ${item.timesSelected} раз`;
+
+                        return (
+                            <div key={item.code} className="List__item">
+                                <div
+                                    className={
+                                        'Item' +
+                                        (item.selected ? ' Item_selected' : '')
+                                    }
+                                    onClick={() => store.selectItem(item.code)}
+                                >
+                                    <div className="Item__number">
+                                        {item.code}
+                                    </div>
+                                    <div className="Item__title">
+                                        {item.title}{' '}
+                                        {item.timesSelected > 0 &&
+                                            selectedTimesContent}
+                                    </div>
+                                    <div className="Item__actions">
+                                        <button
+                                            onClick={() =>
+                                                store.deleteItem(item.code)
+                                            }
+                                        >
+                                            Удалить
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/store.js
+++ src/store.js
@@ -1,83 +1,89 @@
 class Store {
-  constructor(initState) {
-    // Состояние приложения (данные)
-    this.state = initState;
-    // Слушатели изменений state
-    this.listners = [];
-  }
-
-  /**
-   * Выбор state
-   * @return {Object}
-   */
-  getState() {
-    return this.state;
-  }
-
-  /**
-   * Установка state
-   * @param newState {Object}
-   */
-  setState(newState) {
-    this.state = newState;
-    // Оповещаем всех подписчиков об изменении стейта
-    for (const lister of this.listners) {
-      lister();
+    constructor(initState) {
+        // Состояние приложения (данные)
+        this.state = initState;
+        // Слушатели изменений state
+        this.listners = [];
     }
-  }
 
-  /**
-   * Подписка на изменение state
-   * @param callback {Function}
-   * @return {Function} Функция для отписки
-   */
-  subscribe(callback) {
-    this.listners.push(callback);
-    // Возвращаем функцию для удаления слушателя
-    return () => {
-      this.listners = this.listners.filter((item) => item !== callback);
-    };
-  }
+    /**
+     * Выбор state
+     * @return {Object}
+     */
+    getState() {
+        return this.state;
+    }
 
-  /**
-   * Создание записи
-   */
-  createItem({ code, title = 'Новая запись', selected = false }) {
-    this.setState({
-      ...this.state,
-      items: this.state.items.concat({ code, title, selected }),
-    });
-  }
-
-  /**
-   * Удаление записи по её коду
-   * @param code
-   */
-  deleteItem(code) {
-    this.setState({
-      ...this.state,
-      items: this.state.items.filter((item) => item.code !== code),
-    });
-  }
-
-  /**
-   * Выделение записи по её коду
-   * @param code
-   */
-  selectItem(code) {
-    this.setState({
-      ...this.state,
-      items: this.state.items.map((item) => {
-        if (item.code === code) {
-          item.selected = true;
-          item.timesSelected ? item.timesSelected++ : (item.timesSelected = 1);
-        } else {
-          item.selected = false;
+    /**
+     * Установка state
+     * @param newState {Object}
+     */
+    setState(newState) {
+        this.state = newState;
+        // Оповещаем всех подписчиков об изменении стейта
+        for (const lister of this.listners) {
+            lister();
         }
-        return item;
-      }),
-    });
-  }
+    }
+
+    /**
+     * Подписка на изменение state
+     * @param callback {Function}
+     * @return {Function} Функция для отписки
+     */
+    subscribe(callback) {
+        this.listners.push(callback);
+        // Возвращаем функцию для удаления слушателя
+        return () => {
+            this.listners = this.listners.filter((item) => item !== callback);
+        };
+    }
+
+    /**
+     * Создание записи
+     */
+    createItem({ code, title = 'Новая запись', selected = false }) {
+        this.setState({
+            ...this.state,
+            items: this.state.items.concat({ code, title, selected }),
+        });
+    }
+
+    /**
+     * Удаление записи по её коду
+     * @param code
+     */
+    deleteItem(code) {
+        this.setState({
+            ...this.state,
+            items: this.state.items.filter((item) => item.code !== code),
+        });
+    }
+
+    /**
+     * Выделение записи по её коду
+     * @param code
+     */
+    selectItem(code) {
+        this.setState({
+            ...this.state,
+            items: this.state.items.map((item) => {
+                if (item.code === code) {
+                    if (!item.selected) {
+                        item.selected = true;
+                        item.timesSelected
+                            ? item.timesSelected++
+                            : (item.timesSelected = 1);
+                    } else {
+                        item.selected = false;
+                    }
+                } else if (item.selected) {
+                    item.selected = false;
+                }
+                return item;
+            }),
+        });
+    }
 }
 
 export default Store;

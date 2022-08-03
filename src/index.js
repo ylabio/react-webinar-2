858a1a7dc870c5c5a33efc3dd667cd80717501c4
tovach <red5.dalv@gmail.com>
2022-08-03 13:23:03 +0300

    feat: 1 task completed

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app.js";
 import Store from "./store.js";
-import {counter} from './utils.js';
+import { counter } from "./utils.js";
 
 const store = new Store({
   items: [
-    {code: counter(), title: 'Название элемента'},
-    {code: counter(), title: 'Некий объект'},
-    {code: counter(), title: 'Заголовок'},
-    {code: counter(), title: 'Короткое название'},
-    {code: counter(), title: 'Запись'},
-    {code: counter(), title: 'Пример выделенной записи', selected: true},
-    {code: counter(), title: 'Седьмой'},
-  ]
+    { code: counter(), title: "Название элемента", counter: 0 },
+    { code: counter(), title: "Некий объект", counter: 0 },
+    { code: counter(), title: "Заголовок", counter: 0 },
+    { code: counter(), title: "Короткое название", counter: 0 },
+    { code: counter(), title: "Запись", counter: 0 },
+    {
+      code: counter(),
+      title: "Пример выделенной записи",
+      selected: true,
+      counter: 0,
+    },
+    { code: counter(), title: "Седьмой", counter: 0 },
+  ],
 });
 
 // Реакция на изменение store - повторный рендер приложения
 store.subscribe(() => {
-  ReactDOM.render(<App store={store}/>, document.body);
+  ReactDOM.render(<App store={store} />, document.body);
 });
 
 // Первый рендер (один раз)
-ReactDOM.render(<App store={store}/>, document.body);
+ReactDOM.render(<App store={store} />, document.body);

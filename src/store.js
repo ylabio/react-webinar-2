fb314d7e744a04dfc43e1aafbba886e9cb88a088
evgeny3322 <retard_x_98@mail.ru>
2022-08-03 16:49:25 +0300

    fix tasks

--- src/store.js
+++ src/store.js
@@ -1,83 +1,99 @@
 class Store {
 
-  constructor(initState) {
-    // Состояние приложения (данные)
-    this.state = initState;
-    // Слушатели изменений state
-    this.listners = [];
-  }
-
-  /**
-   * Выбор state
-   * @return {Object}
-   */
-  getState() {
-    return this.state;
-  }
-
-  /**
-   * Установка state
-   * @param newState {Object}
-   */
-  setState(newState) {
-    this.state = newState;
-    // Оповещаем всех подписчиков об изменении стейта
-    for (const lister of this.listners) {
-      lister();
+    constructor(initState) {
+        // Состояние приложения (данные)
+        this.state = initState;
+        // Слушатели изменений state
+        this.listners = [];
     }
-  }
 
-  /**
-   * Подписка на изменение state
-   * @param callback {Function}
-   * @return {Function} Функция для отписки
-   */
-  subscribe(callback) {
-    this.listners.push(callback);
-    // Возвращаем функцию для удаления слушателя
-    return () => {
-      this.listners = this.listners.filter(item => item !== callback);
+    /**
+     * Выбор state
+     * @return {Object}
+     */
+    getState() {
+        return this.state;
     }
-  }
 
-  /**
-   * Создание записи
-   */
-  createItem({ code, title = 'Новая запись', counter = 0, selected = false }) {
-    this.setState({
-      ...this.state,
-      items: this.state.items.concat({ code, title, counter, selected })
-    });
-  }
-
-  /**
-   * Удаление записи по её коду
-   * @param code
-   */
-  deleteItem(code) {
-    this.setState({
-      ...this.state,
-      items: this.state.items.filter(item => item.code !== code)
-    });
-  }
-
-  /**
-   * Выделение записи по её коду
-   * @param code
-   */
-  selectItem(code) {
-    this.setState({
-      ...this.state,
-      items: this.state.items.map(item => {
-        item.selected = '';
-        if (item.code === code) {
-          item.selected = !item.selected;
-          item.counter++;
+    /**
+     * Установка state
+     * @param newState {Object}
+     */
+    setState(newState) {
+        this.state = newState;
+        // Оповещаем всех подписчиков об изменении стейта
+        for (const lister of this.listners) {
+            lister();
         }
-        return item;
-      })
-    });
-  }
+    }
+
+    /**
+     * Подписка на изменение state
+     * @param callback {Function}
+     * @return {Function} Функция для отписки
+     */
+    subscribe(callback) {
+        this.listners.push(callback);
+        // Возвращаем функцию для удаления слушателя
+        return () => {
+            this.listners = this.listners.filter(item => item !== callback);
+        }
+    }
+
+    /**
+     * Создание записи
+     */
+    createItem({code, title = 'Новая запись', selected = false, selectedCount}) {
+        this.setState({
+            ...this.state,
+            items: this.state.items.concat({code, title, selected, selectedCount})
+        });
+    }
+
+    /**
+     * Удаление записи по её коду
+     * @param code
+     */
+    deleteItem(code) {
+        this.setState({
+            ...this.state,
+            items: this.state.items.filter(item => item.code !== code)
+        });
+    }
+
+    /**
+     * Выделение записи по её коду
+     * @param code
+     */
+    selectItem(code) {
+        this.setState({
+            ...this.state,
+            items: this.state.items.map(item => {
+                if (item.code === code) {
+                    item.selected = !item.selected
+                } else if (item.code !== code) {
+                    item.selected = false
+                }
+
+                return item;
+            })
+        })
+    }
+
+    setSelectCount(code) {
+        this.setState({
+            ...this.state,
+            items: this.state.items.map(item => {
+                    if (item.code === code && item.selected === false) {
+                        item.selectedCount = item.selectedCount + 1
+                    }
+                    return item;
+                }
+            ),
+        })
+
+    }
 }
+
 
 export default Store;

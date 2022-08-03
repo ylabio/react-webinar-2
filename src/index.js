3aae6005cc4703b1ae8a183002e79c7a897c8854
Nickita <nickita.seomin@yandex.ru>
2022-08-03 17:22:32 +0300

    delete selected from store/items

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import {counter} from './utils.js';
 
 const store = new Store({
   items: [
-    {code: counter(), title: 'Название элемента', counter: 0, selected: false},
-    {code: counter(), title: 'Некий объект', counter: 0, selected: false},
-    {code: counter(), title: 'Заголовок', counter: 0, selected: false},
-    {code: counter(), title: 'Короткое название', counter: 0, selected: false},
-    {code: counter(), title: 'Запись', counter: 0, selected: false},
-    {code: counter(), title: 'Пример выделенной записи', counter: 0, selected: false},
-    {code: counter(), title: 'Седьмой', counter: 0, selected: false},
+    {code: counter(), title: 'Название элемента', counter: 0},
+    {code: counter(), title: 'Некий объект', counter: 0},
+    {code: counter(), title: 'Заголовок', counter: 0},
+    {code: counter(), title: 'Короткое название', counter: 0},
+    {code: counter(), title: 'Запись', counter: 0},
+    {code: counter(), title: 'Пример выделенной записи', counter: 0},
+    {code: counter(), title: 'Седьмой', counter: 0},
   ]
 });
 

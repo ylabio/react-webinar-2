3fa498c4142b26b21a680e0a033a4a981b0b5f8b
DariaMalina <gluto68@gmail.com>
2022-08-03 23:34:56 +0300

    feat: add function to determine the end

--- src/item.jsx
+++ src/item.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 
 function Item({itemInfo: {selected, title, code, counter}, deleteItem, selectItem}) {
-  const numberOfTimes=new Map()
-  const number=[2,3,4,22,23,24,32,33,34]
-  number.forEach(el=>numberOfTimes.set(el,el))
 
-  return <div key={code} className='List__item'>
-    <div className={'Item' + (selected ? ' Item_selected' : '')}
-         onClick={selectItem}>
-      <div className='Item__number'>{code}</div>
-      <div className='Item__title'>{title}{counter > 0 ? ` | Выделялось ${counter} ${numberOfTimes.has(counter)?'раза':'раз'}` : ''}</div>
-      <div className='Item__actions'>
-        <button onClick={deleteItem}>
-          Удалить
-        </button>
-      </div>
+    const correctNumeral = () => {
+        let remainder = counter % 100
+        if (remainder !== 12 && remainder !== 13 && remainder !== 14) {
+            remainder = remainder % 10
+            return remainder === 2 || remainder === 3 || remainder === 4 ? 'раза' : 'раз'
+        }
+        return 'раз'
+    }
+
+    return <div key={code} className='List__item'>
+        <div className={'Item' + (selected ? ' Item_selected' : '')}
+             onClick={selectItem}>
+            <div className='Item__number'>{code}</div>
+            <div
+                className='Item__title'>{title}{counter > 0 ? ` | Выделялось ${counter} ${correctNumeral()}` : ''}</div>
+            <div className='Item__actions'>
+                <button onClick={deleteItem}>
+                    Удалить
+                </button>
+            </div>
+        </div>
     </div>
-  </div>
 }
 
 export default Item

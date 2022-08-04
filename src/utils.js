5917ef8509898ca635337c02664aff0bed30d498
mmigo1 <75628415+mmigo1@users.noreply.github.com>
2022-08-04 13:10:55 +0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -2,22 +2,21 @@
  * Генерирует уникальный код на основе счётчика
  * @returns {number|number}
  */
-export function counter() {
-  return counter.value ? ++counter.value : (counter.value = 1);
+export function counter(){
+  return counter.value ? ++counter.value : counter.value = 1;
 }
-
 export function displaySelected(item) {
-  if (item.counter > 0) {
-    return ` | Выделялся ` + item.counter + getDisplayEnd(item.counter);
-  }
+ if (item.counter > 0) {
+   return item.counter + getDisplayEnd(item.counter);
+ }
+ return false;
 }
-
 function getDisplayEnd(number) {
-  if (number % 100 >= 10 && number % 100 <= 19) {
-    return ` раз`;
-  }
-  if (number % 10 >= 2 && number % 10 <= 4) {
-    return ` раза`;
-  }
-  return ` раз`;
+ if (number % 100 >= 10 && number % 100 <= 19) {
+   return ` раз`;
+ }
+ if (number % 10 >= 2 && number % 10 <= 4) {
+   return ` раза`;
+ }
+ return ` раз`;
 }

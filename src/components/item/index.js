81745484709efd2a7903170e51c52f0e0e64f13a
Дмитрий Бунецкий <bunetskydn@gmail.com>
2022-08-08 14:16:39 +0700

    feat: change the structure and styles of item

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,43 +1,44 @@
-import React, {useCallback} from 'react';
-import propTypes from 'prop-types';
-import {cn as bem} from "@bem-react/classname";
-import './style.css';
+import React from "react";
+import propTypes from "prop-types";
+import { cn as bem } from "@bem-react/classname";
+import "./style.css";
 
 function Item(props) {
-  const cn = bem('Item');
-
-  const callbacks = {
-    onDelete: useCallback((e) => {
-      e.stopPropagation();
-      props.onDelete(props.item.code)
-    }, [props.onDelete,  props.item])
+  const cn = bem("Item");
+  const handlerClick = () => {
+    props.handlerClick(props.item);
   };
-
   return (
-    <div className={cn()} >
-      <div className={cn('number')}>
-        {props.item.code}
-      </div>
-      <div className={cn('title')}>
-        {props.item.title}
-      </div>
-      <div className={cn('actions')}>
-        <button onClick={callbacks.onDelete}>
-          Добавить
-        </button>
+    <div className={cn()}>
+      <div className={cn("number")}>{props.item.code}</div>
+      <div className={cn("title")}>{props.item.title}</div>
+      <div className={cn("price")}>{props.item.price} &#8381;</div>
+
+      {props.item.amount ? (
+        <div className={cn("amount")}>{props.item.amount} шт.</div>
+      ) : (
+        ""
+      )}
+
+      <div className={cn("actions")}>
+        <button onClick={handlerClick}>{props.textButton}</button>
       </div>
     </div>
-  )
+  );
 }
 
 Item.propTypes = {
   item: propTypes.object.isRequired,
-  onSelect: propTypes.func.isRequired,
-  onDelete: propTypes.func.isRequired
-}
+  onDelete: propTypes.func.isRequired,
+  handlerClick: propTypes.func.isRequired,
+  textButton: propTypes.string,
+};
 
 Item.defaultProps = {
-  onDelete: () => {}
-}
+  onDelete: () => {},
+  handlerClick: () => {},
+  textButton: "Кнопка",
+  item: null,
+};
 
 export default React.memo(Item);

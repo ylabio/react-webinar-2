89a5ebebedb56fc0d876b461efdac3112fcb5d8a
spnikit <spnikit@gmail.com>
2022-08-08 16:56:33 +0300

    minor fixes

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -12,6 +12,7 @@ function List(props) {
       {props.items.map(item => (
         <div key={item.code} className={cn('item')}>
           <Item
+            items={props.items}
             item={item}
             onButtonClick={props.onAction}
             cartItem={props.cartItem}

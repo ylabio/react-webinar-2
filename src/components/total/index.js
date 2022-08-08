92413df66a9b37f16aca2aa7c8895cd7c5561f9a
Дмитрий Бунецкий <bunetskydn@gmail.com>
2022-08-08 14:13:48 +0700

    feat: add a component displaying total numbers

---
+++ src/components/total/index.js
@@ -0,0 +1,52 @@
+import React, { useMemo } from "react";
+import propTypes from "prop-types";
+import { cn as bem } from "@bem-react/classname";
+import "./style.css";
+import plural from "plural-ru";
+
+function Total({ list, isShowAmount }) {
+  const cn = bem("Total");
+  const totalPrice = useMemo(() => {
+    return list
+      .map((item) => (item.price && item.amount ? item.price * item.amount : 0))
+      .reduce((prev, value) => prev + value, 0);
+  }, [list]);
+
+  const totalAmount = useMemo(() => {
+    return list.length;
+  }, [list.length]);
+  return (
+    <div className={cn()}>
+      {totalPrice && totalAmount ? (
+        <>
+          <div className={cn("amount")}>
+            {isShowAmount
+              ? `${totalAmount} ${plural(
+                  totalAmount,
+                  "товар",
+                  "товара",
+                  "товаров"
+                )} / `
+              : "Итого"}
+            {}
+          </div>
+          <div className={cn("price")}>{totalPrice} &#8381;</div>
+        </>
+      ) : (
+        "пусто"
+      )}
+    </div>
+  );
+}
+
+Total.propTypes = {
+  list: propTypes.array.isRequired,
+  isShowAmount: propTypes.bool,
+};
+
+Total.defaultProps = {
+  list: [],
+  isShowAmount: false,
+};
+
+export default React.memo(Total);

eda12a82ea0ba3528be1fcba672e493e40c2463c
Star Noob <van.kol@mail.ru>
2022-08-10 00:38:31 +0300

    controls text updated

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -9,7 +9,7 @@ function Controls(props){
 
   return (
     <div className={cn()}>
-      <div className={cn('label')}>В Корзине:{'\t'}
+      <div className={cn('label')}>В корзине:{'\t'}
         <b> {
           props.stats.goods
             ?

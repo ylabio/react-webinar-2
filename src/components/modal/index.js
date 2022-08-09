6c5ef1bf25a2e3b3890ab7441c9755622ad0dba3
Anderboi <upolovnew@gmail.com>
2022-08-09 10:04:30 +0300

    dashed lines added

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -22,20 +22,19 @@ function Modal({active, callModal, cart, callback}) {
 
   return (
 	<div className={cn({'active': active})} onClick={()=>callModal(false)}>
-		<div className={cn('content')} onClick={(e)=> e.stopPropagation()}>
-			<ModalLayout head={
-					<>
-						<h1>Корзина</h1>
-						<Button callback={()=>callModal(false)}>Закрыть</Button>
-					</>} 
-					fullHeight={false}>
-				<List items={cart.cartItems} callback={callback}>{cartListItems}</List>
-				<div className={cn('footer')}>
-						<span>Итого</span>
-						<span>{(cart.totalCost).toLocaleString('ru-RU',{style:'currency', currency:'RUB',maximumFractionDigits: 0})}</span>
-				</div>
-			</ModalLayout>
-		</div>
+	  <div className={cn('content')} onClick={(e)=> e.stopPropagation()}>
+		<ModalLayout head={
+		  <>
+			<h1>Корзина</h1>
+			<Button callback={()=>callModal(false)}>Закрыть</Button>
+		  </>} fullHeight={false}>
+		  <List items={cart.cartItems} callback={callback}>{cartListItems}</List>
+		  <div className={cn('footer')}>
+			<span>Итого</span>
+			<span>{(cart.totalCost).toLocaleString('ru-RU',{style:'currency', currency:'RUB',maximumFractionDigits: 0})}</span>
+		  </div>
+		</ModalLayout>
+	  </div>
 	</div>
   )
 }

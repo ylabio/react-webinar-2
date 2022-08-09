a3a4b0a116c4fcb6249dca187fd2edff19273782
Maksim Zakharov <Yamyamyami97@gmail.com>
2022-08-10 00:57:52 +0300

    Title fix

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ function App({ store }) {
 
   return (
     <>
-      <Layout head={<h1>Приложение на чистом JS</h1>}>
+      <Layout head={<h1>Магазин</h1>}>
         <Controls
           showModal={callbacks.showModal}
           allItems={store.getState().cart.length}

d7de4c15271bf156145aaf09ac52c82e484b1274
TobyThorneA <igraman_XX@mail.ru>
2022-08-09 18:04:57 +0300

    Сделал плюрализацию

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './style.css';
 import {cn as bem} from "@bem-react/classname";
+import { declOfNum } from '../../utils';
 
+const words = ['товар', 'товара', 'товаров']
+declOfNum
 function Controls({getCartStats, onOpen}){
   const cn = bem('Controls');
   return (
     <div className={cn()}>
       <div className={cn('stats')}>
-        В корзине: <strong>{getCartStats().count ? `${getCartStats().count} товара / ${getCartStats().sumPrice.toLocaleString('Ru-ru')} ₽` : `пусто`}</strong>
+        В корзине: <strong>{getCartStats().count ? `${getCartStats().count} ${declOfNum(getCartStats().count, words)} / ${getCartStats().sumPrice.toLocaleString('Ru-ru')} ₽` : `пусто`}</strong>
       </div>
       <button onClick={onOpen}>Перейти</button>
     </div>

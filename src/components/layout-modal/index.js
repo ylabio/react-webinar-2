ca5bde16a9116f303c1e929f8a686c6c1a9019a3
Vladimir Shestakov <boolive@yandex.ru>
2022-08-09 11:10:59 +0300

    scrollbar for modal

--- src/components/layout-modal/index.js
+++ src/components/layout-modal/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import PropTypes from 'prop-types';
 import {cn as bem} from "@bem-react/classname";
 import './style.css';
 
 function LayoutModal(props) {
   const cn = bem('LayoutModal');
+
+  const frame = useRef();
+
+  useEffect(() => {
+    let top = 10;
+    if (window.innerWidth > frame.current.clientHeight) {
+      top = Math.max(top, (window.innerHeight - frame.current.clientHeight) / 2 - top);
+    }
+    frame.current.style.marginTop = `${top}px`;
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = 'auto';
+    }
+  });
+
   return (
     <div className={cn()}>
-      <div className={cn('frame')}>
+      <div className={cn('frame')} ref={frame}>
         <div className={cn('head')}>
           <h1 className={cn('title')}>
             {props.title}

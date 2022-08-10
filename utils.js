4e998e8537ea94e650b6a08d2a8ca66e7f0a14db
Ксения Даниленко <lariksun@yandex.ru>
2022-08-10 22:40:30 +0300

    изменен utils

---
+++ utils.js
@@ -0,0 +1,10 @@
+/**
+ * Генерирует уникальный код на основе счётчика
+ * @returns {number|number}
+ */
+export function counter(){
+  return counter.value ? ++counter.value : counter.value = 1;
+}
+
+
+

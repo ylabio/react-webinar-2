7587efd0e71f6cd856ad88e68299df21f744dbfc
unknown <m_igor_r@mail.ru>
2022-08-10 09:15:10 +0700

    fix design cart

--- src/components/modal/ModalHead.jsx
+++ src/components/modal/ModalHead.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ModalHead = ({ setModalStatus }) => {
    return (
       <div className='Modal-head'>
-         <h1>Корзина</h1>
+         <h1 className='Modal-head-title'>Корзина</h1>
          <button onClick={() => setModalStatus()}>Закрыть</button>
       </div>
    )

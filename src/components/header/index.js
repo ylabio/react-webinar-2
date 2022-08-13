2dce0f4fb202f52ed634ed93e8e9defce50d546c
crzoukman <zoukman@yandex.ru>
2022-08-13 17:21:19 +0300

    fix: add translation for the project title

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,6 +2,7 @@ import React, { useCallback } from "react";
 import {cn as bem} from '@bem-react/classname';
 import './style.css';
 import propTypes from 'prop-types';
+import Translate from "../../app/translate";
 
 function Header({title, changeLanguage, lang}) {
   const cn = bem('Header');
@@ -20,7 +21,7 @@ function Header({title, changeLanguage, lang}) {
 
   return (
     <header className={cn()}>
-      <h1>{title}</h1>
+      <h1><Translate text={title} /></h1>
       <select 
         onChange={callbacks.switchLang}
         value={lang === 'ru' ? 'Русский' : 'English'}

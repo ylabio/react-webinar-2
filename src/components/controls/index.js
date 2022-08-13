adf0614d68f2c18eb6d911491833a1c95e3c882a
Irina <myr-irina2@yandex.ru>
2022-08-13 17:11:52 +0300

    add fixes

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -1,27 +1,21 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import './style.css';
-import { currencyFormat } from '../../utils';
-import plural from 'plural-ru';
-import { cn as bem } from '@bem-react/classname';
 
-function Controls({totalPrice, totalAmount, onPopupOpen}){
-  const cn = bem('Controls');
-  const modifiedCurrency = currencyFormat(totalPrice, 0);
-
-   return (
+function Controls({onAdd}){
+  return (
     <div className='Controls'>
-      <div className={cn('wrapper')}><p className={cn('text')}>В корзине:</p><b>{totalAmount ? `${totalAmount} ${plural(totalAmount, 'товар', 'товара', 'товаров')} / ${modifiedCurrency}` : 'пусто'}</b></div>
-      <button className={cn('actions')} onClick={onPopupOpen}>Перейти</button>
+      <button onClick={onAdd}>Добавить</button>
     </div>
   )
 }
 
-
 Controls.propTypes = {
-  totalPrice: propTypes.number.isRequired,
-  totalAmount: propTypes.number.isRequired,
-  onPopupOpen: propTypes.func.isRequired // Обяхательное свойство - функция
+  onAdd: propTypes.func.isRequired // Обяхательное свойство - функция
+}
+
+Controls.defaultProps = {
+  onAdd: () => {} // Значение по умолчанию - функция-заглушка
 }
 
 export default React.memo(Controls);

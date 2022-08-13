af739135a5233f0bfa73308fc7533b4eea94b8fb
a0ne <mako666777@gmail.com>
2022-08-14 00:06:46 +0300

    fixed-basket-items

--- src/components/item-basket/index.js
+++ src/components/item-basket/index.js
@@ -2,19 +2,22 @@ import React, {useCallback} from 'react';
 import propTypes from 'prop-types';
 import numberFormat from "../../utils/number-format";
 import {cn as bem} from "@bem-react/classname";
+import { useNavigate } from 'react-router-dom'
 import './styles.css';
 
 function ItemBasket(props) {
   const cn = bem('ItemBasket');
+    const navigate = useNavigate()
 
   const callbacks = {
-    onRemove: useCallback((e) => props.onRemove(props.item._id), [props.onRemove,  props.item])
+    onRemove: useCallback((e) => props.onRemove(props.item._id), [props.onRemove,  props.item]),
+    onNavigate: useCallback(() => navigate(`item/${props.item._id}`, {replace: true}), [props.item])
   };
 
   return (
     <div className={cn()}>
       {/*<div className={cn('id')}>{props.item._id}</div>*/}
-      <div className={cn('title')}>{props.item.title}</div>
+      <div className={cn('title')} onClick={callbacks.onNavigate}>{props.item.title}</div>
       <div className={cn('right')}>
         <div className={cn('cell')}>{numberFormat(props.item.price)} ₽</div>
         <div className={cn('cell')}>{numberFormat(props.item.amount || 0)} шт</div>

0bc18a63ce1d12931640eded1cab69bb0c99982d
fabralbert <fabrmonk@gmail.com>
2022-08-14 16:31:04 +0300

    rewrite unit-test

--- src/utils/createPagination/createPagination.test.js
+++ src/utils/createPagination/createPagination.test.js
@@ -1,151 +1,55 @@
 import createPagination from "./createPagination";
 
-describe('1 элемент активный', () => {
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(1,1)).toEqual([1]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(1,2)).toEqual([1,2]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(1,3)).toEqual([1,2,3]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(1,4)).toEqual([1,2,3,4]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(1,5)).toEqual([1,2,3,4,5]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(1,6)).toEqual([1,2,3,null,6]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(1,7)).toEqual([1,2,3,null,7]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(1,8)).toEqual([1,2,3,null,8]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(1,9)).toEqual([1,2,3,null,9]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(1,10)).toEqual([1,2,3,null,10]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(1,100)).toEqual([1,2,3,null,100]);
-  })
-})
+const list = [
+  [1,1,[1]], 
+  [1,2,[1,2]],
+  [1,3,[1,2,3]],
+  [1,4,[1,2,3,4]],
+  [1,5,[1,2,3,4,5]],
+  [1,6,[1,2,3,null,6]],
+  [1,7,[1,2,3,null,7]],
+  [1,8,[1,2,3,null,8]],
+  [1,9,[1,2,3,null,9]],
+  [1,10,[1,2,3,null,10]],
+  [1,100,[1,2,3,null,100]],
+  [2,2,[1,2]],
+  [2,3,[1,2,3]],
+  [2,4,[1,2,3,4]],
+  [2,5,[1,2,3,4,5]],
+  [2,6,[1,2,3,null,6]],
+  [2,7,[1,2,3,null,7]],
+  [2,8,[1,2,3,null,8]],
+  [2,9,[1,2,3,null,9]],
+  [2,10,[1,2,3,null,10]],
+  [2,100,[1,2,3,null,100]],
+  [3,3,[1,2,3]],
+  [3,4,[1,2,3,4]],
+  [3,5,[1,2,3,4,5]],
+  [3,6,[1,2,3,4,null,6]],
+  [3,7,[1,2,3,4,null,7]],
+  [3,8,[1,2,3,4,null,8]],
+  [3,9,[1,2,3,4,null,9]],
+  [3,10,[1,2,3,4,null,10]],
+  [3,100,[1,2,3,4,null,100]],
+  [4,4,[1,2,3,4]],
+  [4,5,[1,2,3,4,5]],
+  [4,6,[1,null,3,4,5,6]],
+  [4,7,[1,null,3,4,5,null,7]],
+  [4,8,[1,null,3,4,5,null,8]],
+  [4,9,[1,null,3,4,5,null,9]],
+  [4,10,[1,null,3,4,5,null,10]],
+  [4,100,[1,null,3,4,5,null,100]],
+  [5,5,[1,2,3,4,5]],
+  [5,6,[1,null,4,5,6]],
+  [5,7,[1,null,4,5,6,7]],
+  [5,8,[1,null,4,5,6,null,8]],
+  [5,9,[1,null,4,5,6,null,9]],
+  [5,10,[1,null,4,5,6,null,10]],
+  [5,100,[1,null,4,5,6,null,100]],
+]
 
-describe('2 элемент активный', () => {
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(2,2)).toEqual([1,2]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(2,3)).toEqual([1,2,3]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(2,4)).toEqual([1,2,3,4]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(2,5)).toEqual([1,2,3,4,5]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(2,6)).toEqual([1,2,3,null,6]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(2,7)).toEqual([1,2,3,null,7]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(2,8)).toEqual([1,2,3,null,8]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(2,9)).toEqual([1,2,3,null,9]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(2,10)).toEqual([1,2,3,null,10]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(2,100)).toEqual([1,2,3,null,100]);
-  })
-})
-
-describe('3 элемент активный', () => {
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(3,3)).toEqual([1,2,3]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(3,4)).toEqual([1,2,3,4]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(3,5)).toEqual([1,2,3,4,5]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(3,6)).toEqual([1,2,3,4,null,6]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(3,7)).toEqual([1,2,3,4,null,7]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(3,8)).toEqual([1,2,3,4,null,8]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(3,9)).toEqual([1,2,3,4,null,9]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(3,10)).toEqual([1,2,3,4,null,10]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(3,100)).toEqual([1,2,3,4,null,100]);
-  })
-})
-
-describe('4 элемент активный', () => {
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(4,4)).toEqual([1,2,3,4]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(4,5)).toEqual([1,2,3,4,5]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(4,6)).toEqual([1,null,3,4,5,6]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(4,7)).toEqual([1,null,3,4,5,null,7]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(4,8)).toEqual([1,null,3,4,5,null,8]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(4,9)).toEqual([1,null,3,4,5,null,9]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(4,10)).toEqual([1,null,3,4,5,null,10]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(4,100)).toEqual([1,null,3,4,5,null,100]);
-  })
-})
-
-describe('5 элемент активный', () => {
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(5,5)).toEqual([1,2,3,4,5]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(5,6)).toEqual([1,null,4,5,6]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(5,7)).toEqual([1,null,4,5,6,7]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(5,8)).toEqual([1,null,4,5,6,null,8]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(5,9)).toEqual([1,null,4,5,6,null,9]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(5,10)).toEqual([1,null,4,5,6,null,10]);
-  })
-  test('Проверка значения на выходе', () => {
-    expect(createPagination(5,100)).toEqual([1,null,4,5,6,null,100]);
+list.forEach((item) => {
+  test(`Активный: ${item[0]}, всего страниц: ${item[1]} `, () => {
+    expect(createPagination(item[0],item[1])).toEqual(item[2]);
   })
 })

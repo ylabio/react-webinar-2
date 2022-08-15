74f4b75653c4b2356839f30dbe38243da3dbfe77
serge <spnikit@gmail.com>
2022-08-15 19:42:24 +0300

    close modal when go via link in basket

--- src/components/item-details/index.js
+++ src/components/item-details/index.js
@@ -1,45 +1,42 @@
-import React, {useCallback, useEffect} from 'react';
-import {cn as bem} from '@bem-react/classname';
-import {useParams} from 'react-router-dom';
-import './style.css';
-import useSelector from '../../utils/use-selector';
-import ItemCard from '../item-card';
-import Layout from '../layout';
-import BasketSimple from '../basket-simple';
-import useStore from '../../utils/use-store';
+import React, { useCallback, useEffect } from "react";
+import { cn as bem } from "@bem-react/classname";
+import { useParams } from "react-router-dom";
+import "./style.css";
+import useSelector from "../../utils/use-selector";
+import ItemCard from "../item-card";
+import Layout from "../layout";
+import BasketSimple from "../basket-simple";
+import useStore from "../../utils/use-store";
 
 function ItemDetails() {
-  console.log('ItemDetails');
-  const cn = bem('ItemDetails');
+  console.log("ItemDetails");
+  const cn = bem("ItemDetails");
 
   const params = useParams();
   const store = useStore();
 
   useEffect(() => {
-    store.get('itemDetails').loadItem(params.itemId);
+    store.get("itemDetails").loadItem(params.itemId);
+    store.get("modals").close();
   }, [params.itemId]);
 
-  const select = useSelector(state => ({
+  const select = useSelector((state) => ({
     sum: state.basket.sum,
     amount: state.basket.amount,
-    selectedItem: state.itemDetails.selectedItem
+    selectedItem: state.itemDetails.selectedItem,
   }));
 
   const callbacks = {
     // Открытие корзины
-    openModalBasket: useCallback(() => store.get('modals').open('basket'), []),
+    openModalBasket: useCallback(() => store.get("modals").open("basket"), []),
     // Добавление в корзину
-    addToBasket: useCallback(_id => store.get('basket').addToBasket(_id), [])
+    addToBasket: useCallback((_id) => store.get("basket").addToBasket(_id), []),
   };
 
   return (
     <div className={cn()}>
       <Layout head={<h1>{select.selectedItem?.title}</h1>}>
-        <BasketSimple
-          onOpen={callbacks.openModalBasket}
-          amount={select.amount}
-          sum={select.sum}
-        />
+        <BasketSimple onOpen={callbacks.openModalBasket} amount={select.amount} sum={select.sum} />
         <ItemCard
           description={select.selectedItem.description}
           originCountry={select.selectedItem?.maidIn?.title}
@@ -48,7 +45,7 @@ function ItemDetails() {
           yearOfProduction={select.selectedItem?.edition}
           price={select.selectedItem?.price}
           onAdd={() => {
-            callbacks.addToBasket(select.selectedItem['_id']);
+            callbacks.addToBasket(select.selectedItem["_id"]);
           }}
         />
       </Layout>

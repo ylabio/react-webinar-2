61828ebe7f93582d2563ac5ce7b1f3c055f15bd1
valed-dm <dmvaled@gmail.com>
2022-08-15 12:42:44 +0300

    убрал пробелы у скобок {useNavigate}

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,5 +1,5 @@
 import React, {useCallback} from 'react';
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import propTypes from 'prop-types';
 import {cn as bem} from "@bem-react/classname";
 import numberFormat from "../../utils/numberFormat";

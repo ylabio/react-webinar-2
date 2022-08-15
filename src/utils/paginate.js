363bd9d551f1899d5fd2a5a43810aeaa99a877ff
Your Name <you@example.com>
2022-08-15 12:44:47 +0300

    fix function paginate

--- src/utils/paginate.js
+++ src/utils/paginate.js
@@ -1,7 +1,7 @@
 //Функция, возвращающая массив для пагинации
 export const paginate = (arr, page) => {
   let arrPagination;
-  if (arr.length > 5) {
+  if (arr.length >= 5) {
     if (page < 2) {
       arrPagination =[...arr.slice(0, 3), '...', arr[arr.length-1]];
     } else if (page === 2) {

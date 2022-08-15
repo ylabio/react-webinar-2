2b9baac08e7a26c420df30c1bbb8207de8b37f73
serge <spnikit@gmail.com>
2022-08-16 00:50:48 +0300

    fix issue when error happened while adding item to the basket in item details component after refresh

--- src/store/basket/index.js
+++ src/store/basket/index.js
@@ -1,10 +1,9 @@
-import StateModule from "../module";
+import StateModule from '../module';
 
 /**
  * Состояние корзины
  */
-class BasketState extends StateModule{
-
+class BasketState extends StateModule {
   /**
    * Начальное состояние
    * @return {Object}
@@ -12,8 +11,8 @@ class BasketState extends StateModule{
   initState() {
     return {
       items: [],
-        sum: 0,
-        amount: 0
+      sum: 0,
+      amount: 0
     };
   }
 
@@ -21,7 +20,7 @@ class BasketState extends StateModule{
    * Добавление товара в корзину
    * @param _id Код товара
    */
-  addToBasket(_id) {
+  async addToBasket(_id) {
     let sum = 0;
     // Ищем товар в корзие, чтобы увеличить его количество. Заодно получаем новый массив items
     let exists = false;
@@ -34,25 +33,37 @@ class BasketState extends StateModule{
       }
       // Добавляея в общую сумму
       sum += result.price * result.amount;
-      return result
+      return result;
     });
 
     // Если товар не был найден в корзине, то добавляем его из каталога
     if (!exists) {
       // Поиск товара в каталоге, чтобы его в корзину добавить
       // @todo В реальных приложения будет запрос к АПИ на добавление в корзину, и апи выдаст объект товара..
-      const item = this.store.getState().catalog.items.find(item => item._id === _id);
+      let item;
+      if (this.store.getState().catalog.items.length < 1) {
+        const response = await fetch(`/api/v1/articles/${_id}`);
+        const json = await response.json();
+        item = json.result;
+      } else {
+        item = this.store
+          .getState()
+          .catalog.items.find(item => item._id === _id);
+      }
       items.push({...item, amount: 1});
       // Досчитываем сумму
       sum += item.price;
     }
 
     // Установка состояние, basket тоже нужно сделать новым
-    this.setState({
-      items,
-      sum,
-      amount: items.length
-    }, 'Добавление в корзину');
+    this.setState(
+      {
+        items,
+        sum,
+        amount: items.length
+      },
+      'Добавление в корзину'
+    );
   }
 
   /**
@@ -63,16 +74,19 @@ class BasketState extends StateModule{
     let sum = 0;
     const items = this.getState().items.filter(item => {
       // Удаляемый товар
-      if (item._id === _id) return false
+      if (item._id === _id) return false;
       // Подсчёт суммы если твоар не удаляем.
       sum += item.price * item.amount;
       return true;
     });
-    this.setState({
-      items,
-      sum,
-      amount: items.length
-    }, 'Удаление из корзины')
+    this.setState(
+      {
+        items,
+        sum,
+        amount: items.length
+      },
+      'Удаление из корзины'
+    );
   }
 }
 

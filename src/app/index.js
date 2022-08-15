4d7199d2495edc518ef6c3bc5319eb4fdb7a8e3c
valed-dm <dmvaled@gmail.com>
2022-08-15 12:07:41 +0300

    удалены неиспользуемые импорты в app

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Main from "./main";
 import Basket from "./basket";

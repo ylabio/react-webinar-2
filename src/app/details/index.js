6228ebb729a1aa8a45cc2e491e4ca5ae06eb7b66
Star Noob <van.kol@mail.ru>
2022-08-16 14:44:48 +0300

    added store module 'item' and fixed F5 basket bug

--- src/app/details/index.js
+++ src/app/details/index.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect } from "react";
 import { useParams } from 'react-router-dom';
 import BasketSimple from "../../components/basket-simple";
 import ItemDetails from '../../components/item-details';
@@ -10,11 +10,12 @@ function Details(props) {
 
   const { id } = useParams();
   const store = useStore();
-  const [info, setInfo] = useState(null);
 
   const select = useSelector(state => ({
     amount: state.basket.amount,
-    sum: state.basket.sum
+    sum: state.basket.sum,
+    items: state.catalog.items,
+    info: state.item.fields
   }));
 
   const callbacks = {
@@ -24,21 +25,21 @@ function Details(props) {
     addToBasket: useCallback(_id => store.get('basket').addToBasket(_id), []),
   };
 
-  // Подробности по итему используются только здесь, вытаскиваем через промисы
+  // Подробности по итему
   useEffect(() => {
-    fetch(`api/v1/articles/${id}?fields=*,maidIn(title,code),category(title)`)
+    /* fetch(`api/v1/articles/${id}?fields=*,maidIn(title,code),category(title)`)
       .then(response => response.json()
-        .then(json => setInfo(json.result))
-      )
+        .then(json => { setInfo(json.result); })) */
+
+    store.get('item').load(id);
   }, [id]);
 
-  //console.log("Deatails for item " + id + " :", info);
-  //if (!info) return null;
+  //console.log("Deatails for item " + id + " :", select.info);
 
   return (
-    <Layout head={<h1>{info?.title}</h1>}>
+    <Layout head={<h1>{select.info?.title}</h1>}>
       <BasketSimple onOpen={callbacks.openModalBasket} amount={select.amount} sum={select.sum} />
-      {info ? <ItemDetails onAdd={callbacks.addToBasket} info={info} /> : null}
+      {select.info?.title ? <ItemDetails onAdd={callbacks.addToBasket} info={select.info} /> : null}
     </Layout>
   );
 };
--- src/store/exports.js
+++ src/store/exports.js
@@ -1,4 +1,6 @@
 export { default as basket } from './basket';
 export { default as catalog } from './catalog';
+export { default as item } from './item';
 export { default as localization } from './localization';
 export { default as modals } from './modals';
+

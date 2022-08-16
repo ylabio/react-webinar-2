6c474d8140eaf89a697076d43c09ddca44055239
Nikolay Rzhevsky <ayyge@LAPTOP-0U39RI1M.(none)>
2022-08-16 08:45:11 +0300

    fix adding item from cart after refresh

--- src/components/item-details/index.js
+++ src/components/item-details/index.js
@@ -16,23 +16,23 @@ const ItemDetails = ({ item, onAdd }) => {
 
   return (
     <div className={cn()}>
-      <div className={cn('description')}>{item?.result?.description}</div>
+      <div className={cn('description')}>{item.result?.description}</div>
       <div className={cn('maidIn')}>
         <div>Страна производитель:</div>
-        <div>{item?.result?.maidIn.title}</div>
-        <div>({item?.result?.maidIn.code})</div>
+        <div>{item.result?.maidIn.title}</div>
+        <div>({item.result?.maidIn.code})</div>
       </div>
       <div className={cn('category')}>
         <div>Категория: </div>
-        <div>{item?.result?.category.title}</div>
+        <div>{item.result?.category.title}</div>
       </div>
       <div className={cn('edition')}>
         <div>Год выпуска: </div>
-        <div>{item?.result?.edition}</div>
+        <div>{item.result?.edition}</div>
       </div>
       <div className={cn('price')}>
         <div>Цена: </div>
-        <div>{numberFormat(item?.result?.price)} ₽</div>
+        <div>{numberFormat(item.result?.price)} ₽</div>
       </div>
       <button className={cn('actions')} onClick={callbacks.onAdd}>
         Добавить

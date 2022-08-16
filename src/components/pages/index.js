a48dee9f162dbb836b1afca21e60c1ec26be0e26
Kirill Tikhonov <lliriq@me.com>
2022-08-16 18:05:15 +0300

    Edit pages

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { NavLink } from "react-router-dom";
+import {NavLink, useParams} from "react-router-dom";
 import propTypes from 'prop-types';
 import {cn as bem} from "@bem-react/classname";
 import './style.css';
@@ -7,26 +7,25 @@ import './style.css';
 function Pages(props){
 const pagesList = [];
 const cn = bem('Pages');
-const [active, setActive] = useState(0);
+const {query} = useParams();
+const [active, setActive] = useState((query) ? query.split('=')[2]/10 : 0);
 
 for (let n = 1; n <= props.count; n++) {
     pagesList.push(n);
-} 
+}
 
 return (
     <div className={cn()}>
-        {pagesList.map((item, index) =>
-        <NavLink key={item}
-        className={({isActive}) =>{
-            if (index === 0 && active === 0) return cn('item_active');
-            else if (index === 0) return isActive ? cn('item_active') : cn('item');
-            else return cn('item_none');
+        {
+        <NavLink className={({isActive}) =>{
+          if (active === 0) return cn('item_active');
+          else return isActive ? cn('item_active') : cn('item');
         }}
-        onClick={()=>setActive(index)}
-        to={`limit=${props.perPage}&skip=${item*props.perPage-props.perPage}`}>
-            {item}
+        onClick={()=>setActive(0)}
+        to={`limit=${props.perPage}&skip=${1*props.perPage-props.perPage}`}>
+          {1}
         </NavLink>
-        )}
+        }
 
         {(active > 2) && <span className={cn('spread')}>...</span>}
 
@@ -50,18 +49,15 @@ return (
 
       {(active < (props.count - 3)) && <span className={cn('spread')}>...</span>}
 
-      {pagesList.map((item, index) =>
-        <NavLink key={item}
-        className={({isActive}) =>{
-            if (index === (props.count - 1) && active === (props.count - 1)) return cn('item_active');
-            else if (index === (props.count - 1)) return isActive ? cn('item_active') : cn('item');
-            else return cn('item_none');
+      {
+        <NavLink className={({isActive}) =>{
+            return isActive ? cn('item_active') : cn('item');
         }}
-        onClick={()=>setActive(index)}
-        to={`limit=${props.perPage}&skip=${item*props.perPage-props.perPage}`}>
-            {item}
+        onClick={()=>setActive(props.count - 1)}
+        to={`limit=${props.perPage}&skip=${props.count*props.perPage-props.perPage}`}>
+            {(props.count) ? props.count : ''}
         </NavLink>
-        )}
+        }
     </div>
 )
 }

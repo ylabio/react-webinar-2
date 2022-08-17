b4cb5fa31e3db5d16b24dda9bc2ff587d7355a87
Maksim Zakharov <Yamyamyami97@gmail.com>
2022-08-17 18:38:38 +0300

    propTypes

--- src/components/item-basket/index.js
+++ src/components/item-basket/index.js
@@ -6,37 +6,44 @@ import './styles.css';
 import {Link} from "react-router-dom";
 
 function ItemBasket(props) {
-  const cn = bem('ItemBasket');
+    const cn = bem('ItemBasket');
 
 
-  const callbacks = {
-    onRemove: useCallback(() => props.onRemove(props.item._id), [props.onRemove,  props.item]),
-  };
+    const callbacks = {
+        onRemove: useCallback(() => props.onRemove(props.item._id), [props.onRemove, props.item]),
+    };
 
-  return (
-    <div className={cn()}>
-      {/*<div className={cn('id')}>{props.item._id}</div>*/}
-      <div className={cn('title')} ><Link to={props.link} onClick={() => props.closeModal()}>
-          {props.item.title}
-      </Link>
-      </div>
-      <div className={cn('right')}>
-        <div className={cn('cell')}>{numberFormat(props.item.price)} ₽</div>
-        <div className={cn('cell')}>{numberFormat(props.item.amount || 0)} шт</div>
-        <div className={cn('cell')}><button onClick={callbacks.onRemove}>Удалить</button></div>
-      </div>
-    </div>
-  )
+    return (
+        <div className={cn()}>
+            {/*<div className={cn('id')}>{props.item._id}</div>*/}
+            <div className={cn('title')}><Link to={props.link} onClick={() => props.closeModal()}>
+                {props.item.title}
+            </Link>
+            </div>
+            <div className={cn('right')}>
+                <div className={cn('cell')}>{numberFormat(props.item.price)} ₽</div>
+                <div className={cn('cell')}>{numberFormat(props.item.amount || 0)} шт</div>
+                <div className={cn('cell')}>
+                    <button onClick={callbacks.onRemove}>Удалить</button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 ItemBasket.propTypes = {
-  item: propTypes.object.isRequired,
-  onRemove: propTypes.func,
-    link: propTypes.string
+    item: propTypes.object.isRequired,
+    onRemove: propTypes.func,
+    link: propTypes.string,
+    closeModal: propTypes.func
 }
 
 ItemBasket.defaultProps = {
-link: ''
+    link: '',
+    onRemove: () => {
+    },
+    closeModal: () => {
+    }
 }
 
 export default React.memo(ItemBasket);

8a37875ca768a7cea033ec1fa2dfefca2a967619
Elena Generalova <generalm88@gmail.com>
2022-08-20 05:39:17 +0300

    Feat: add profile page

--- src/app/profile/index.js
+++ src/app/profile/index.js
@@ -1,10 +1,6 @@
-import React, { useCallback } from "react";
-import useStore from "../../hooks/use-store";
+import React from "react";
 import useSelector from "../../hooks/use-selector";
-import { useParams } from "react-router-dom";
-import useInit from "../../hooks/use-init";
 import useTranslate from "../../hooks/use-translate";
-import LoginForm from "../../components/login-form";
 import Spinner from "../../components/spinner";
 import Tools from "../../containers/tools";
 import Layout from "../../components/layout";
@@ -14,26 +10,10 @@ import UserInfo from "../../components/user-info";
 import TopMenu from "../../containers/top-menu";
 
 function Profile() {
-  const store = useStore();
-
-  // Параметры из пути /articles/:id
-  const params = useParams();
-
-  useInit(async () => {
-    await store.get("article").load(params.id);
-  }, [params.id]);
-
   const select = useSelector((state) => ({
-    article: state.article.data,
-    waiting: state.article.waiting,
+    user: state.auth.user,
   }));
-
   const { t } = useTranslate();
-
-  const callbacks = {
-    // Добавление в корзину
-    addToBasket: useCallback((_id) => store.get("basket").addToBasket(_id), []),
-  };
 
   return (
     <>
@@ -47,7 +27,7 @@ function Profile() {
         }
       >
         <Tools />
-        <UserInfo />
+        <UserInfo user={select.user} t={t} />
       </Layout>
     </>
   );
--- src/components/user-info/index.js
+++ src/components/user-info/index.js
@@ -1,14 +1,38 @@
 import React from "react";
 import propTypes from "prop-types";
 import { cn as bem } from "@bem-react/classname";
-import numberFormat from "../../utils/number-format";
 import "./style.css";
 
-function UserInfo({}) {
+function UserInfo({ user, t }) {
   // CSS классы по БЭМ
-  const cn = bem("LoginForm");
+  const cn = bem("UserInfo");
 
-  return <div className={cn()}>Профиль имя телефон адрес</div>;
+  return (
+    <div className={cn()}>
+      <h2>{t("userInfo.title")}</h2>
+      <div className={cn("prop")}>
+        <div className={cn("label")}>{t("userInfo.name")}:</div>
+        <div className={cn("value")}>{user.username}</div>
+      </div>
+      <div className={cn("prop")}>
+        <div className={cn("label")}>{t("userInfo.phone")}:</div>
+        <div className={cn("value")}>{user.profile?.phone}</div>
+      </div>
+      <div className={cn("prop")}>
+        <div className={cn("label")}>email:</div>
+        <div className={cn("value")}>{user.email}</div>
+      </div>
+    </div>
+  );
 }
+UserInfo.propTypes = {
+  user: propTypes.object.isRequired,
+  t: propTypes.func,
+};
+
+UserInfo.defaultProps = {
+  user: {},
+  t: () => {},
+};
 
 export default React.memo(UserInfo);

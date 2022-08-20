defc25cfc5b8ebc90468223a650567020f152a35
Artem Dmytrishyn <gamegir@gmail.com>
2022-08-20 20:30:48 +0300

    fix clickable pagination dots

--- src/components/pagination/index.js
+++ src/components/pagination/index.js
@@ -34,14 +34,17 @@ function Pagination(props) {
 
   return (
     <ul className={cn()}>
-      {items.map((number, index) => (
-        <li key={index}
-            className={cn('item', {active: number === props.page, split: !number})}
-            onClick={onClickHandler(number)}
-        >
-          {number || '...'}
-        </li>
-      ))}
+      {items.map((number, index) => 
+        number ?
+          <li key={index}
+              className={cn('item', {active: number === props.page, split: !number})}
+              onClick={onClickHandler(number)}
+          >
+            {number}
+          </li>
+        :
+          <span className={cn('dots')}>...</span>
+      )}
     </ul>
   )
 }

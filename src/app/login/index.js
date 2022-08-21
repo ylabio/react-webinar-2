847910c4bf073d443b61ab00ccda36f09ace3fa8
Leonid660 <leonid_zolotovky@bk.ru>
2022-08-21 14:35:16 +0300

    lecture4

---
+++ src/app/login/index.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useCallback } from "react";
+import useStore from "../../hooks/use-store";
+import useSelector from "../../hooks/use-selector";
+import { useNavigate } from "react-router-dom";
+import useTranslate from "../../hooks/use-translate";
+import Spinner from "../../components/spinner";
+import Tools from "../../containers/tools";
+import Layout from "../../components/layouts/layout";
+import LayoutFlex from "../../components/layouts/layout-flex";
+import LocaleSelect from "../../containers/locale-select";
+import LoginForm from "../../components/login-form";
+import User from "../../containers/user";
+
+function Login() {
+  const { t } = useTranslate();
+  const store = useStore();
+  const navigate = useNavigate();
+
+  const select = useSelector((state) => ({
+    token: state.user.token,
+    waiting: state.user.waiting,
+    error: state.user.error,
+  }));
+
+  useEffect(() => {
+    // Редирект на страницу профиля, если пользователь авторизирован
+    if (select.token) {
+      navigate("/profile", { replace: true });
+    }
+  }, [select.token]);  
+
+  const callbacks = {
+    // Функция авторизации
+    onLogin: useCallback(
+      (login, password) => store.get("user").onLogin(login, password),
+      []
+    ),
+  };
+
+  return (
+    <Layout
+      before={
+        <LayoutFlex flex="end" padding={false}>
+          <User />
+        </LayoutFlex>
+      }
+      head={
+        <LayoutFlex flex="between">
+          <h1>{t("title")}</h1>
+          <LocaleSelect />
+        </LayoutFlex>
+      }
+    >
+      <Tools />
+      <Spinner active={select.waiting}>
+        <LoginForm onLogin={callbacks.onLogin} error={select.error} t={t} />
+      </Spinner>
+    </Layout>
+  );
+}
+
+export default React.memo(Login);
---
+++ src/app/profile/index.js
@@ -0,0 +1,52 @@
+import React, { useEffect } from "react";
+import useSelector from "../../hooks/use-selector";
+import { useNavigate } from "react-router-dom";
+import useTranslate from "../../hooks/use-translate";
+import Spinner from "../../components/spinner";
+import Tools from "../../containers/tools";
+import Layout from "../../components/layouts/layout";
+import LayoutFlex from "../../components/layouts/layout-flex";
+import LocaleSelect from "../../containers/locale-select";
+import User from "../../containers/user";
+import ProfileCard from "../../components/profile-card";
+
+function Profile() {
+  const { t } = useTranslate();
+  const navigate = useNavigate();
+
+  const select = useSelector((state) => ({
+    token: state.user.token,
+    waiting: state.user.waiting,
+    user: state.user.data,
+  }));
+
+  useEffect(() => {
+    // Редирект на страницу логина, если пользователь не авторизирован
+    if (!select.token) {
+      navigate("/login", { replace: true });
+    }
+  }, [select.token]);
+
+  return (
+    <Layout
+      before={
+        <LayoutFlex flex="end" padding={false}>
+          <User />
+        </LayoutFlex>
+      }
+      head={
+        <LayoutFlex flex="between">
+          <h1>{t("title")}</h1>
+          <LocaleSelect />
+        </LayoutFlex>
+      }
+    >
+      <Tools />
+      <Spinner active={select.waiting}>
+        <ProfileCard user={select.user} t={t} />
+      </Spinner>
+    </Layout>
+  );
+}
+
+export default React.memo(Profile);

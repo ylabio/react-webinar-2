903e6b0db0a3e8af0fb44a66a6d8d064764cb02d
Lexxich <opium4free@gmail.com>
2022-08-21 15:10:16 +0300

    checkpoint before getting categories list

--- src/containers/catalog-filter/index.js
+++ src/containers/catalog-filter/index.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useMemo} from "react";
+import React, { useCallback, useMemo } from "react";
 import useSelector from "../../hooks/use-selector";
 import useStore from "../../hooks/use-store";
 import useTranslate from "../../hooks/use-translate";
@@ -7,40 +7,65 @@ import Input from "../../components/input";
 import LayoutFlex from "../../components/layout-flex";
 
 function CatalogFilter() {
-
   const store = useStore();
 
-  const select = useSelector(state => ({
+  const select = useSelector((state) => ({
     sort: state.catalog.params.sort,
     query: state.catalog.params.query,
+    filter: state.catalog.params.filter,
   }));
 
-  const {t} = useTranslate();
+  const { t } = useTranslate();
 
   const callbacks = {
     // Сортировка
-    onSort: useCallback(sort => store.get('catalog').setParams({sort}), []),
+    onSort: useCallback((sort) => store.get("catalog").setParams({ sort }), []),
+    onFilter: useCallback(
+      (filter) => store.get("catalog").setParams({ filter }),
+      []
+    ),
     // Поиск
-    onSearch: useCallback(query => store.get('catalog').setParams({query, page: 1}), []),
+    onSearch: useCallback(
+      (query) => store.get("catalog").setParams({ query, page: 1 }),
+      []
+    ),
     // Сброс
-    onReset: useCallback(() => store.get('catalog').resetParams(), [])
+    onReset: useCallback(() => store.get("catalog").resetParams(), []),
   };
 
   // Опции для полей
   const options = {
-    sort: useMemo(() => ([
-      {value:'order', title: 'По порядку'},
-      {value:'title.ru', title: 'По именованию'},
-      {value:'-price', title: 'Сначала дорогие'},
-      {value:'edition', title: 'Древние'},
-    ]), [])
-  }
+    sort: useMemo(
+      () => [
+        { value: "order", title: "По порядку" },
+        { value: "title.ru", title: "По именованию" },
+        { value: "-price", title: "Сначала дорогие" },
+        { value: "edition", title: "Древние" },
+      ],
+      []
+    ),
+    filter: useMemo(() => [{ value: "Test", title: "Test" }], []),
+  };
 
   return (
     <LayoutFlex flex="start">
-      <Select onChange={callbacks.onSort} value={select.sort} options={options.sort}/>
-      <Input onChange={callbacks.onSearch} value={select.query} placeholder={'Поиск'} theme="big"/>
-      <button onClick={callbacks.onReset}>{t('filter.reset')}</button>
+      <Select
+        onChange={callbacks.onSort}
+        value={select.sort}
+        options={options.sort}
+      />
+      <Select
+        onChange={callbacks.onFilter}
+        value={select.filter}
+        options={options.filter}
+      />
+      <Input
+        onChange={callbacks.onSearch}
+        value={select.query}
+        placeholder={"Поиск"}
+        theme="big"
+      />
+      <button onClick={callbacks.onReset}>{t("filter.reset")}</button>
     </LayoutFlex>
   );
 }

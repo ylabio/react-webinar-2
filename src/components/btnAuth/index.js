63cf12d43fa04e2e2a0761cf03052c47d309a87a
Vladislav <webproject67@gmail.com>
2022-08-22 10:25:30 +0300

    кнопка авторизвции вынес в отдельный компонент

---
+++ src/components/btnAuth/index.js
@@ -0,0 +1,46 @@
+import React, { useCallback } from 'react';
+import { useNavigate, Link } from "react-router-dom";
+import { cn as bem } from "@bem-react/classname";
+import LayoutFlex from "../layout-flex";
+import { getToken } from "../../services/token";
+import propTypes from "prop-types";
+import './style.css';
+
+function BtnAuth({t, name, removeToken}){
+  const cn = bem('BtnAuth');
+
+  const navigate = useNavigate();
+
+  const callbacks = {
+    handleButtonClick: useCallback(() => navigate(`/login`), []),
+    handleButtonExitClick: useCallback(() => removeToken(), [])
+  };
+
+  return (
+    <div className={cn()}>
+      <LayoutFlex flex="end">
+        {getToken() && <Link className={cn('link')} to='/profile'>{name}</Link>}
+        <button 
+          type='button' 
+          onClick={name ? 
+            callbacks.handleButtonExitClick : 
+            callbacks.handleButtonClick
+          }
+        >
+          {name ? t('exit') : t('entery')}
+        </button>
+      </LayoutFlex>
+    </div>
+  )
+}
+
+BtnAuth.propTypes = {
+  name: propTypes.string,
+  t: propTypes.func,
+  removeToken: propTypes.func
+}
+
+BtnAuth.defaultProps = {
+}
+
+export default React.memo(BtnAuth);
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,36 +1,15 @@
-import React, { useCallback } from 'react';
-import { useNavigate, Link } from "react-router-dom";
+import React from 'react';
 import {cn as bem} from "@bem-react/classname";
-import LayoutFlex from "../layout-flex";
-import {getToken} from "../../services/token";
-
+import BtnAuth from "../btnAuth";
 import propTypes from "prop-types";
 import './style.css';
 
 function Layout({t, name, removeToken, head, children}){
   const cn = bem('Layout');
 
-  const navigate = useNavigate();
-
-  const callbacks = {
-    handleButtonClick: useCallback(() => navigate(`/login`), []),
-    handleButtonExitClick: useCallback(() => removeToken(), [])
-  };
-
   return (
     <div className={cn()}>
-      <LayoutFlex flex="end">
-        {getToken() && <Link className={cn('link')} to='/profile'>{name}</Link>}
-        <button 
-          type='button' 
-          onClick={name ? 
-            callbacks.handleButtonExitClick : 
-            callbacks.handleButtonClick
-          }
-        >
-          {name ? t('exit') : t('entery')}
-        </button>
-      </LayoutFlex>
+      <BtnAuth t={t} name={name} removeToken={removeToken}/>
       <div className={cn('head')}>
         {head}
       </div>

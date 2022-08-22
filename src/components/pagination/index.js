bd4fa3343523e181c956fcc6f767291c25f3d9d7
Saveliy <svolskey2929@gmail.com>
2022-08-22 18:53:35 +0300

    добавил ссылки в пагинацию(доп задание)

--- src/components/pagination/index.js
+++ src/components/pagination/index.js
@@ -2,18 +2,19 @@ import React from 'react';
 import propTypes from "prop-types";
 import {cn as bem} from '@bem-react/classname'
 import './style.css';
+import {Link} from "react-router-dom";
 
 function Pagination(props) {
   const cn = bem('Pagination');
   // Количество страниц
   const length = Math.ceil(props.count / Math.max(props.limit, 1));
-
+  
   // Номера слева и справа относительно активного номера, которые остаются видимыми
   let left = Math.max(props.page - props.indent, 1);
   let right = Math.min(left + props.indent * 2, length);
   // Корректировка когда страница в конце
   left = Math.max(right - props.indent * 2, 1);
-
+  
   // Массив номеров, чтобы удобней рендерить
   let items = [];
   // Первая страница всегда нужна
@@ -26,21 +27,23 @@ function Pagination(props) {
   if (right < length - 1) items.push(null);
   // Последнаяя страница
   if (right < length) items.push(length);
-
+  
   // Возвращает функцию с замыканием на номер страницы
   const onClickHandler = page => {
     return () => props.onChange(page);
   };
-
+  
   return (
     <ul className={cn()}>
       {items.map((number, index) => (
-        <li key={index}
-            className={cn('item', {active: number === props.page, split: !number})}
-            onClick={onClickHandler(number)}
-        >
-          {number || '...'}
-        </li>
+        <Link to={`/${window.location.search}`}>
+          <li key={index}
+              className={cn('item', {active: number === props.page, split: !number})}
+              onClick={onClickHandler(number)}
+          >
+            {number || '...'}
+          </li>
+        </Link>
       ))}
     </ul>
   )

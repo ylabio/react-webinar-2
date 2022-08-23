2b375ff195d7cb1b0971563e3b3e1ea40218d774
Leonid660 <leonid_zolotovky@bk.ru>
2022-08-23 18:42:39 +0300

    добавил катёгорию CСюрприз :)

--- src/store/categories/index.js
+++ src/store/categories/index.js
@@ -25,8 +25,10 @@ class CategoriesState extends StateModule{
             waiting: true,
         });
         // Загрузка категорий
-        const response = await fetch(`/api/v1/categories`);
+        const response = await fetch(`/api/v1/categories?limit=*`);
+
         const json = await response.json();
+        console.log(json)
         // Преобразование в массив с префиксами дочерних элементов
         const items = [{value: '', title: 'Все'}, ...categoriesToLabel(json.result.items)]
         this.setState({

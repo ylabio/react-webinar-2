c0093f8dca6cee776b7623325e355c4772e7438e
Alex <x3m.f@mail.ru>
2022-08-23 15:20:53 +0400

    fix: add space for dash

--- src/utils/categories.js
+++ src/utils/categories.js
@@ -7,7 +7,7 @@ export function fn (arr, parent, result) {
       parent = {...parent, hash: parent.hash ? parent.hash : ''};
 
       const childs = arr.filter(child => child.parent && (child.parent._id === parent._id))
-        .map(child => ({...child, hash: child.hash || '-' + parent.hash }));
+        .map(child => ({...child, hash: child.hash || '- ' + parent.hash }));
 
       const res = childs.reduce((acc, item) => [...acc, ...fn(arr, item, result)], []);
       return fn(arr, undefined, [...result, ...([parent, ...res])]);

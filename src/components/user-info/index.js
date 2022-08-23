ff630c0d5068cb7a37bc6d4c03a74f005cab301e
satori <nullsatori@gmail.com>
2022-08-23 11:20:17 +0300

    fix name at profile page

--- src/components/user-info/index.js
+++ src/components/user-info/index.js
@@ -5,13 +5,13 @@ import propTypes from "prop-types";
 
 const UserInfo = ({user, t}) => {
   const cn = bem('User');
-  
+  console.log(user);
   return (
     <div className={cn()}>
       <h2>{t("user.profile")}</h2>
       <div className={cn('wrap')}>
         <label className={cn('label')}>{t("user.name")}:</label>
-        <div className={cn('value')}>{user && user.username}</div>
+        <div className={cn('value')}>{user && user.profile?.name}</div>
       </div>
       <div className={cn('wrap')}>
         <label className={cn('label')}>{t("user.phone")}:</label>

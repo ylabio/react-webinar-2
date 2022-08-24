351a7a6b3c88432e967c8d7a61f15bd6983c777e
valed-dm <dmvaled@gmail.com>
2022-08-24 20:32:26 +0300

    общие замечания - профиль - в целях навигации изменен  ProfilePage

--- src/components/profile-page/index.js
+++ src/components/profile-page/index.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
 import propTypes from "prop-types";
 import {cn as bem} from '@bem-react/classname';
 import './style.css';
 
 function ProfilePage({t, user}) {
   const cn = bem('ProfilePage');
+  const navigate = useNavigate();
 
-  return (
-    <div className={cn()}>
-      <h2>{t('profile')}</h2>
-      <div className={cn('user-data')}>
-        <div className={cn('cell')}>Имя:&nbsp;
-          <span className={cn('bold')}>{user ? user.profile?.name : null}</span>
+  {
+    if (user.profile) {
+      return (
+        <div className={cn()}>
+          <h2>{t('profile')}</h2>
+          <div className={cn('user-data')}>
+            <div className={cn('cell')}>Имя:&nbsp;
+              <span className={cn('bold')}>{user ? user.profile?.name : null}</span>
+            </div>
+            <div className={cn('cell')}>Телефон:&nbsp;
+              <span className={cn('bold')}>{user ? user.profile?.phone : null}</span></div>
+            <div className={cn('cell')}>email:&nbsp;
+              <span className={cn('bold')}>{user ? user?.email : null}</span></div>
+          </div>
         </div>
-        <div className={cn('cell')}>Телефон:&nbsp;
-          <span className={cn('bold')}>{user ? user.profile?.phone : null}</span></div>
-        <div className={cn('cell')}>email:&nbsp;
-          <span className={cn('bold')}>{user ? user?.email : null}</span></div>
-      </div>
-    </div>
-  )
+      )
+    } else {
+      useEffect(() => {
+        navigate('/')
+      }, [user])
+    }
+  }
 }
 
 ProfilePage.propTypes = {

67d84df8cc9af2e0306f8644e12258e1e2564fdc
WilhelmYakunin <v.jakunin@inbox.ru>
2022-08-24 13:25:11 +0300

    fix bugs

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,5 @@
-import React, { useMemo } from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../store/authcontext';
 import useSelector from "../hooks/use-selector";
 import {Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from './login';
@@ -14,12 +15,17 @@ import Article from "./article";
 function App() {
 
   const modal = useSelector(state => state.modals.name);
+  const { user } = useContext(AuthContext);
+  console.log(user)
+  const userToken = localStorage.token || user;
 
   return (
     <>
       <Routes>
         <Route path='/login' element={<LoginPage />}/>
-        <Route path='/profile/:userId' element={<UserPage />}/>
+        <Route path='/profile/:userId' element={
+          !userToken ? <Navigate replace to='/login'/> 
+          : <UserPage />}/>
         <Route path={''} element={<Main/>}/>
         <Route path={"/articles/:id"} element={<Article/>}/>
       </Routes>

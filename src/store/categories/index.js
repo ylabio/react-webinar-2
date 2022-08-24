f9c7e944cd60a9bb60072b5bdced8466a55dffd8
Юрий Архипов <arhipow.yuri@gmail.com>
2022-08-25 04:27:27 +0600

    change categories method

--- src/store/categories/index.js
+++ src/store/categories/index.js
@@ -15,37 +15,33 @@ class CategoriesState extends StateModule{
     async loadCategories() {
         const response = await fetch(`api/v1/categories?limit=*`);
         const json = await response.json();
-        const createTreeData = (arr) => {
-          const tree = Object.fromEntries(arr.map(item => [ item._id, { ...item, parent_id: item.parent?._id, children: [] } ]));
-          return Object.values(tree).filter(item => !tree[item.parent_id]?.children.push(item));
+        
+        const createTree = (array) => {
+            let tree = []
+                for (let i = 0; i < array.length; i++) {
+                    array[i].children = []
+                    if (array[i].parent) {
+                        let parent = array.filter(elem => elem._id === array[i].parent._id).pop()
+                        parent.children.push(array[i])
+                    } else {
+                        tree.push(array[i])
+                    }
+                }
+            return tree
         };
 
-    
+        const res = [];
         function formateTree(arr, prefix = '') {
-            let res = [];
-            //     arr.forEach(obj => {
-            //         obj.value = obj._id
-            //         obj.title = prefix + obj.title
-            //         if (obj.children) {
-            //             formateTree(obj.children, prefix + '- ')
-            //         }
-            //         res.push(obj)
-            //     })
-
-            arr.forEach((item) => {
-              res = [
-                ...res,
-                { value: item._id, title: prefix + item.title },
-                ...(item.children
-                ? formateTree(item.children, prefix + "- ")
-                : {}),
-              ];
-            });
+            arr.forEach( item => {
+                res.push({value: item._id, title: prefix + item.title})
+                if (item.children) {
+                    formateTree(item.children,  prefix + "- ") 
+                }  
+            })
             return res;
         }
-        
     
-        const allCategories = [{value: '', title: 'Все'}, ...formateTree(createTreeData(json.result.items))]
+        const allCategories = [{value: '', title: 'Все'}, ...formateTree(createTree(json.result.items))]
         this.setState({
           ...this.getState(),
           categories: allCategories

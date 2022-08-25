b33cbc9a30f86a9f1697b58d01139b79c1b9166a
Anton <tosha.kondratenko@yandex.ru>
2022-08-25 15:10:56 +0300

    loginBar на страницу товара и ридерект через авторизацию

--- src/app/autho/index.js
+++ src/app/autho/index.js
@@ -10,28 +10,35 @@ import useTranslate from "../../hooks/use-translate";
 import LoginForm from "../../containers/login-form";
 import useStore from "../../hooks/use-store";
 import useInit from "../../hooks/use-init";
+import { useLocation } from 'react-router-dom';
 
 
 function Authorization() {
+
+    const location = useLocation();
 
     const navigate = useNavigate();
 
     const { t } = useTranslate();
 
     const select = useSelector(state => ({
-        user: state.authorization.name
+        user: state.authorization.name,
+        productID: state.article.data._id
     }));
+
+
 
     const store = useStore();
 
-    useInit(() => {
-        (select.user && navigate('/'));
+    useInit(async () => {
+        await store.get('authorization').choiceProfile();
+        ((select.user && location.state === "product") && navigate(`/articles/${select.productID}`));
+        ((select.user && !location.state) && navigate('/'));
     }, [select.user]);
 
     const callback = {
         exit: useCallback(() => store.get('authorization').logOut(), []),
     }
-
 
     return (<Fragment>
         <LoginBar userName={select.user} logOut={callback.exit} />
--- src/components/login-bar/index.js
+++ src/components/login-bar/index.js
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 import './style.css';
 
 
-function LoginBar({ userName, logOut }) {
+function LoginBar({ statePage, userName, logOut }) {
 
     return (
         <div className="bar-panel">
             <Link to="/profile"><p className="bar-name">{userName}</p></Link>
-            {userName ? <button onClick={() => logOut()} className="bar-button">Выход</button> : <Link to="/login"><button className="bar-button">Вход</button></Link>}
+            {userName ? <button onClick={() => logOut()} className="bar-button">Выход</button> : <Link state={statePage} to="/login"><button className="bar-button">Вход</button></Link>}
         </div>
 
     )

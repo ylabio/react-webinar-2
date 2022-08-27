ff6c1884d96da660b7ed90a2f02d1b15f48b88f0
Timur Kim <kimutir@gmail.com>
2022-08-27 13:28:26 +1100

    1

---
+++ src/store-redux/comments/actions.js
@@ -0,0 +1,92 @@
+export default {
+  load: (_id) => {
+    return async (dispatch, getState, services) => {
+      dispatch({ type: 'comments/load' });
+      try {
+        const json = await services.api.request({
+          url: `/api/v1/comments?search[parent]=${_id}&fields=_id,text,dateCreate,parent,author(profile(name))&limit=*`,
+        });
+        dispatch({
+          type: 'comments/load-success',
+          payload: {
+            data: json.result.items.map((i) => {
+              i.active = false;
+              return i;
+            }),
+          },
+        });
+      } catch (e) {
+        // Ошибка при загрузке
+        dispatch({ type: 'comments/load-error' });
+      }
+    };
+  },
+
+  post: (data, userName, _id, comId) => {
+    return async (dispatch, getState, services) => {
+      try {
+        await services.api.request({
+          method: 'POST',
+          url: '/api/v1/comments',
+          body: JSON.stringify(data),
+        });
+
+        // загружем новый комментарий через запрос
+        const json = await services.api.request({
+          url: `/api/v1/comments?search[parent]=${_id}&fields=_id,text,dateCreate,parent,author(profile(name))&limit=*`,
+        });
+        dispatch({
+          type: 'comments/load-success',
+          payload: {
+            data: json.result.items.map((i) => {
+              if (i._id !== comId) {
+                i.active = false;
+              } else {
+                i.active = true;
+              }
+              return i;
+            }),
+          },
+        });
+        // новый комментарий добавляем вручную
+        // dispatch({
+        //   type: 'comments/add',
+        //   payload: {
+        //     data: {
+        //       ...json.result,
+        //       author: { profile: { name: userName } },
+        //       active: false,
+        //     },
+        //   },
+        // });
+        console.log('json:', json.result);
+      } catch (e) {}
+    };
+  },
+
+  show: (id) => {
+    return (dispatch, getState, services) => {
+      const com = getState().comments.data;
+      com.map((i) => {
+        if (i._id === id) {
+          i.active = true;
+        } else {
+          i.active = false;
+        }
+        return i;
+      });
+      dispatch({ type: 'comments/show', payload: [...com] });
+    };
+  },
+
+  hide: () => {
+    return (dispatch, getState, services) => {
+      const com = getState().comments.data;
+      com.map((i) => {
+        i.active = false;
+        return i;
+      });
+      dispatch({ type: 'comments/show', payload: [...com] });
+    };
+  },
+};
--- src/utils/list-to-tree/index.js
+++ src/utils/list-to-tree/index.js
@@ -4,11 +4,11 @@
  * @param key {String} Свойство с первичным ключём
  * @returns {Array} Корневые узлы
  */
-export default function listToTree(list, key = '_id') {
+export default function listToTree(list, key = '_id', parentId = null) {
   let trees = {};
   let roots = {};
-  for (const item of list) {
 
+  for (const item of list) {
     // Добавление элемента в индекс узлов с создание свойства children
     if (!trees[item[key]]) {
       trees[item[key]] = item;
@@ -19,14 +19,27 @@ export default function listToTree(list, key = '_id') {
       trees[item[key]] = Object.assign(trees[item[key]], item);
     }
 
-    // Если элемент имеет родителя, то добавляем его в подчиенные родителя
-    if (item.parent?._id) {
-      // Если родителя ещё нет в индексе, то индек созадётся, ведь _id родителя известен
-      if (!trees[item.parent._id]) trees[item.parent._id] = { children: [] };
-      // Добавления в подчиенные родителя
-      trees[item.parent._id].children.push(trees[item[key]]);
-      // Так как элемент добавлен к родителю, то он уже не является корневым
-      if (roots[item[key]]) delete roots[item[key]];
+    if (parentId) {
+      // Если элемент имеет родителя, то добавляем его в подчиенные родителя
+      if (item.parent?._id !== parentId) {
+        // Если родителя ещё нет в индексе, то индек созадётся, ведь _id родителя известен
+        if (!trees[item.parent._id]) trees[item.parent._id] = { children: [] };
+        // Добавления в подчиенные родителя
+        trees[item.parent._id].children.push(trees[item[key]]);
+        // Так как элемент добавлен к родителю, то он уже не является корневым
+        if (roots[item[key]]) delete roots[item[key]];
+      }
+    }
+    if (!parentId) {
+      // Если элемент имеет родителя, то добавляем его в подчиенные родителя
+      if (item.parent?._id) {
+        // Если родителя ещё нет в индексе, то индек созадётся, ведь _id родителя известен
+        if (!trees[item.parent._id]) trees[item.parent._id] = { children: [] };
+        // Добавления в подчиенные родителя
+        trees[item.parent._id].children.push(trees[item[key]]);
+        // Так как элемент добавлен к родителю, то он уже не является корневым
+        if (roots[item[key]]) delete roots[item[key]];
+      }
     }
   }
   return Object.values(roots);

a7eec4de8e29ae02c2ee80744769af9e9956a1d3
Yaroslav <ravlushevich.iarek@mail.ru>
2022-08-27 11:07:16 +0300

    add redux-devtools-extension

--- src/store-redux/index.js
+++ src/store-redux/index.js
@@ -1,9 +1,10 @@
 import {createStore, combineReducers, applyMiddleware} from "redux";
 import thunk from 'redux-thunk';
 import * as reducers from './exports';
+import { composeWithDevTools } from "redux-devtools-extension";
 
 export default function createStoreRedux(services, config){
-  return createStore(combineReducers(reducers), undefined, applyMiddleware(
+  return createStore(combineReducers(reducers), composeWithDevTools(applyMiddleware(
     thunk.withExtraArgument(services)
-  ));
+  )));
 }

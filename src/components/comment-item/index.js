7ce4e7c1b7f5e9f85d4ab8b2504d465650d74e5b
Ivan <duremar98@yandex.ru>
2022-08-28 12:47:06 +0300

    date

--- src/components/comment-item/index.js
+++ src/components/comment-item/index.js
@@ -1,18 +1,26 @@
 import React from "react";
 import propTypes from "prop-types";
 import { cn as bem } from "@bem-react/classname";
+import dayjs from "dayjs";
+import "dayjs/locale/ru";
 import "./styles.css";
 
 function CommentItem({ item }) {
   const cn = bem("Comment");
 
   return (
-    <div className={cn()}>
+    <div
+      style={{ paddingLeft: `${(item.parent._tree.length - 1) * 20}px` }}
+      className={cn()}
+    >
       <div className={cn("name")}>
-        <h5>User1</h5> <span>{item.dateCreate}</span>
+        <h5>User1</h5>{" "}
+        <span>
+          {dayjs(item.dateCreate).locale("ru").format("D MMMM YYYY в h:mm")}
+        </span>
       </div>
       <div className={cn("text")}>{item.text}</div>
-      <div>
+      <div className={cn("answer")}>
         <p>Ответить</p>
       </div>
     </div>

492e3194be08bd8cabc955c85b61a616b350cc34
serge <spnikit@gmail.com>
2022-08-29 13:26:26 +0300

    move back to comments when redirect to login in order to comment

--- src/components/info-badge/index.js
+++ src/components/info-badge/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import {cn as bem} from '@bem-react/classname';
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 import './style.css';
 
 function InfoBadge(props) {
   const cn = bem('InfoBadge');
+  const location = useLocation();
 
   return (
     <div className={cn()}>
-      <Link to={'/login'}>Войдите</Link>, чтобы иметь возможность комментировать
+      <Link to={'/login'} state={{back: location.pathname}}>
+        Войдите
+      </Link>
+      , чтобы иметь возможность комментировать
     </div>
   );
 }

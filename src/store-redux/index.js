086d9235f8403ea613af5d90535b120e59249393
Simon <skorencem@gmail.com>
2022-08-29 22:59:20 +1000

    fix

--- src/store-redux/index.js
+++ src/store-redux/index.js
@@ -3,7 +3,5 @@ import thunk from 'redux-thunk';
 import * as reducers from './exports';
 
 export default function createStoreRedux(services, config) {
-  return createStore(combineReducers(reducers), undefined, compose(applyMiddleware(
-    thunk.withExtraArgument(services)
-  ), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));  //девтулс редакс, включатся только если есть расширение браузера
+  return createStore(combineReducers(reducers), undefined, compose(applyMiddleware(thunk.withExtraArgument(services))));
 }

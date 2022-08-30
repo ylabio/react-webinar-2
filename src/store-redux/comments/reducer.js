47d62c5176b9a1ff6ad8f378054a433a4da2d193
Eugene Shumkov <radoxmgn@gmail.com>
2022-08-30 20:39:42 +0300

    scroll to new comment

--- src/store-redux/comments/reducer.js
+++ src/store-redux/comments/reducer.js
@@ -6,14 +6,15 @@ const initialState = {
   data: [],
   count: 0,
   waiting: false,
-  error : '',
+  comment: '',
+  error: '',
 }
 
-export default function reducer(state = initialState, action){
+export default function reducer(state = initialState, action) {
   switch (action.type) {
 
     case "comments/load":
-      return { ...state, data: [], count: 0, waiting: true, error : '',};
+      return { ...state, data: [], count: 0, waiting: true, error: '', comment: '', };
 
     case "comments/load-success":
       return {
@@ -21,49 +22,56 @@ export default function reducer(state = initialState, action){
         data: [
           ...treeToList(
             listToTree(action.payload.data),
-            (item, level) => ({...item, active: false, level: level, main: false})
+            (item, level) => ({ ...item, active: false, level: level, main: false })
           ),
         ],
         count: action.payload.count,
-        waiting: false};
+        waiting: false,
+        comment: '',
+      };
 
     case "comments/load-error":
-      return { ...state, data: [], count: 0, waiting: false, error : action.payload.error};
+      return { ...state, data: [], count: 0, waiting: false, error: action.payload.error, comment: '', };
 
     case "comments/add-comment":
-      return { ...state, waiting: true, error : '',};
+      return { ...state, waiting: true, error: '', comment: '', };
 
     case "comments/add-comment-success":
       return {
         ...state,
         waiting: false,
+        comment: action.payload._id,
         data: [
-        ...treeToList(
-          listToTree([...state.data, action.payload]),
-          (item, level) => ({...item, level: level, active: false})
-        ),
-    ],
+          ...treeToList(
+            listToTree([...state.data, action.payload]),
+            (item, level) => ({ ...item, level: level, active: false })
+          ),
+        ],
         count: state.count + 1,
       };
 
     case "comments/add-comment-error":
-      return { ...state, waiting: false, error : action.payload.error};
+      return { ...state, waiting: false, error: action.payload.error, comment: '', };
 
     case "comments/answer":
       return {
         ...state,
+        comment: '',
         data: state.data.map(item => {
           item.active = action.payload === item._id;
           return item;
-        })};
+        })
+      };
 
     case "comments/close":
       return {
         ...state,
+        comment: '',
         data: state.data.map(item => {
           item.active = item._id === '0';
           return item;
-        })};
+        })
+      };
 
     default:
       // Нет изменений

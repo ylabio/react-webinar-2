2285da527db9943d1bc3cfab16f5367a5ac8279b
Alexandr <k.a.r.2002@mail.ru>
2022-09-03 17:18:40 +0300

    animation

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import propTypes from 'prop-types';
-import {cn as bem} from "@bem-react/classname";
+import { cn as bem } from "@bem-react/classname";
 import './style.css';
+import { AnimatePresence,motion } from 'framer-motion';
 
 function List(props) {
   const cn = bem('List');
 
   return (
-    <div className={cn()}>{props.items.map(item =>
-      <div key={item._id} className={cn('item')}>
-        {props.renderItem(item)}
+    <AnimatePresence>
+
+      <div className={cn()}>{props.items.map(item =>
+     
+        <motion.div 
+          key={item._id} 
+          className={cn('item')}
+          initial={{scale:0}}
+          animate={{
+            scale:1,
+            transition:{delay:0.7,type:'just'}
+          }}
+          exit={{
+            opacity:0,
+            transition:{delay:0.5}
+          }}
+          layout
+          >
+          {props.renderItem(item)}
+        </motion.div>
+      )}
       </div>
-    )}
-    </div>
+    </AnimatePresence>
+
   )
 }
 

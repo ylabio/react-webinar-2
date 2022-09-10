b51342731413e2a35746eaede3a35b92737e9afe
Nikolay Rzhevsky <ayyge@LAPTOP-0U39RI1M.(none)>
2022-09-10 10:39:08 +0300

    fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', 'http://example.front.ylab.io');
+app.set('port', process.env.PORT);
 
 var server = app.listen(app.get('port'), function () {
   console.log('listening on port ', server.address().port);
